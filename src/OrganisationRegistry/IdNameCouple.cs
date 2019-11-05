9edcf5e739db5baff0333fef2cd992f5411f1cec
David Cumps <david@cumps.be>
2019-11-05 16:22:50 +0100

    refactor: add an id name type #14

---
+++ src/OrganisationRegistry/IdNameCouple.cs
@@ -0,0 +1,28 @@
+namespace OrganisationRegistry
+{
+    using System.Collections.Generic;
+    using Be.Vlaanderen.Basisregisters.AggregateSource;
+
+    public class IdName<TId, TName> : ValueObject<IdName<TId, TName>>
+        where TId : ValueObject<TId>
+        where TName : ValueObject<TName> 
+    {
+        public TId Id { get; }
+
+        public TName Name { get; }
+
+        public IdName(
+            TId id,
+            TName name)
+        {
+            Id = id;
+            Name = name;
+        }
+
+        protected override IEnumerable<object> Reflect()
+        {
+            yield return Id;
+            yield return Name;
+        }
+    }
+}
