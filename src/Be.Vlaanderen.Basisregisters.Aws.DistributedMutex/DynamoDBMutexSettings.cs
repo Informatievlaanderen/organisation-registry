eb1b876f615a590f8b93b77c49856fe0b467c8fa
Koen Metsu <koen@kometsoft.be>
2019-12-18 16:19:34 +0100

    feat: add DistributedMutex

---
+++ src/Be.Vlaanderen.Basisregisters.Aws.DistributedMutex/DynamoDBMutexSettings.cs
@@ -0,0 +1,9 @@
+﻿namespace Be.Vlaanderen.Basisregisters.Aws.DistributedMutex
+{
+    public class DynamoDBMutexSettings
+    {
+        public string TableName { get; set; } = "__lock__";
+
+        public bool CreateTableIfNotExists { get; set; } = false;
+    }
+}
---
+++ src/Be.Vlaanderen.Basisregisters.Aws.DistributedMutex/LockToken.cs
@@ -0,0 +1,26 @@
+﻿namespace Be.Vlaanderen.Basisregisters.Aws.DistributedMutex
+{
+    using System;
+
+    public class LockToken
+    {
+        public string ResourceId { get; }
+
+        public string HolderId { get; }
+
+        public DateTimeOffset LeaseExpiry { get; }
+
+        public LockToken(
+            string holderId,
+            string resourceId,
+            DateTimeOffset leaseExpiry)
+        {
+            ResourceId = resourceId;
+            HolderId = holderId;
+            LeaseExpiry = leaseExpiry;
+        }
+
+        public LockToken WithNewExpiry(DateTimeOffset newExpiry)
+            => new LockToken(HolderId, ResourceId, newExpiry);
+    }
+}
