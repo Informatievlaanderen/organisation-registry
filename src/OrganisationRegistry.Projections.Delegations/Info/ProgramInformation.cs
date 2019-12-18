d6e00ac99610b2414311600eff37df2b9ecee643
Koen Metsu <koen@kometsoft.be>
2019-12-18 16:22:28 +0100

    feat: print settings on run

--- src/OrganisationRegistry.Projections.Delegations/Info/ProgramInformation.cs
+++ src/OrganisationRegistry.Projections.Delegations/Info/ProgramInformation.cs
@@ -2,16 +2,22 @@
 {
     using System.Text;
     using Configuration;
+    using OrganisationRegistry.Infrastructure.Configuration;
 
     public class ProgramInformation
     {
-        public static string Build(DelegationsRunnerConfiguration delegationsRunnerConfiguration)
+        public static string Build(DelegationsRunnerConfiguration delegationsRunnerConfiguration, TogglesConfiguration togglesConfiguration)
         {
             var progInfo = new StringBuilder();
             progInfo.AppendLine();
             progInfo.AppendLine("Application settings:");
             progInfo.AppendLine(new string('-', 50));
-            // print out settings here
+            AppendKeyValue(progInfo, "ApiAvailable", togglesConfiguration.ApiAvailable.ToString());
+            AppendKeyValue(progInfo, "DelegationsRunnerAvailable", togglesConfiguration.DelegationsRunnerAvailable.ToString());
+            progInfo.AppendLine(new string('-', 50));
+            AppendKeyValue(progInfo, "LockRegionEndPoint", delegationsRunnerConfiguration.LockRegionEndPoint);
+            AppendKeyValue(progInfo, "LockTableName", delegationsRunnerConfiguration.LockTableName);
+            AppendKeyValue(progInfo, "LockLeasePeriodInMinutes", delegationsRunnerConfiguration.LockLeasePeriodInMinutes.ToString());
             progInfo.AppendLine(new string('-', 50));
             progInfo.AppendLine();
             return progInfo.ToString();
