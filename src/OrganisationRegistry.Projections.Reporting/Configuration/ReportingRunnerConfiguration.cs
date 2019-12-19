c6090b977666e41bf52bce1444f2766631cf400d
Koen Metsu <koen@kometsoft.be>
2019-12-19 13:08:12 +0100

    feat: use mutex to prevent concurring processing in reporting runner

--- src/OrganisationRegistry.Projections.Reporting/Configuration/ReportingRunnerConfiguration.cs
+++ src/OrganisationRegistry.Projections.Reporting/Configuration/ReportingRunnerConfiguration.cs
@@ -10,6 +10,12 @@
 
         [JsonConverter(typeof(TimestampConverter))]
         public DateTime Created => DateTime.Now;
+
+        public string LockRegionEndPoint { get; set; }
+        public string LockAccessKeyId { get; set; }
+        public string LockAccessKeySecret { get; set; }
+        public string LockTableName { get; set; }
+        public int LockLeasePeriodInMinutes { get; set; }
     }
 
 }
