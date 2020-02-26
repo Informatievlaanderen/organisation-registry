93a56545b0eeb1e493d738a31c674d07464b2a37
Koen Metsu <koen@kometsoft.be>
2020-02-26 11:44:17 +0100

    fix: match response models to actual response

--- src/OrganisationRegistry.UI/app/services/kbo/kbo.model.ts
+++ src/OrganisationRegistry.UI/app/services/kbo/kbo.model.ts
@@ -1,10 +1,14 @@
 import { UUID } from 'angular2-uuid';
 
+class KboName {
+  public value: string = '';
+}
+
 export class Kbo {
   constructor(
     public id: string = UUID.UUID(),
-    public name: string = '',
-    public shortName: string = '',
+    public formalName: KboName = null,
+    public shortName: KboName = null,
     public kboNumber: string = '',
     public description: string = '',
     public validFrom: Date = null,
