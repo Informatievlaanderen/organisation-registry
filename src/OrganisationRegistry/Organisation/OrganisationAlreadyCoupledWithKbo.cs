8a147a4c31c5517766dd54bd94ee8a18090776b5
Koen Metsu <koen@kometsoft.be>
2020-03-05 10:29:02 +0100

    feat: apply OrganisationCoupledWithKbo

---
+++ src/OrganisationRegistry/Organisation/OrganisationAlreadyCoupledWithKbo.cs
@@ -0,0 +1,8 @@
+namespace OrganisationRegistry.Organisation
+{
+    public class OrganisationAlreadyCoupledWithKbo: DomainException
+    {
+        public OrganisationAlreadyCoupledWithKbo()
+            : base("Deze organisatie is reeds gekoppeld aan een kbo nummer.") { }
+    }
+}
