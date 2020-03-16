090a3381b5a19bcca966499968638ad03c5037fd
Koen Metsu <koen@kometsoft.be>
2020-03-16 13:55:39 +0100

    feat: use factory to create ftp client

--- src/OrganisationRegistry.KboMutations/KboMutationsRunnerModule.cs
+++ src/OrganisationRegistry.KboMutations/KboMutationsRunnerModule.cs
@@ -10,7 +10,6 @@ namespace OrganisationRegistry.KboMutations
     using Microsoft.Extensions.Configuration;
     using Microsoft.Extensions.DependencyInjection;
     using Microsoft.Extensions.Logging;
-    using Microsoft.Extensions.Options;
     using SqlServer;
 
     public class KboMutationsRunnerModule : Autofac.Module
