2fb232c018c6a623f48720843b9653cb1e6c47f8
Koen Metsu <koen@kometsoft.be>
2020-03-19 20:14:15 +0100

    feat: add migration to separate status from info

---
+++ src/OrganisationRegistry.SqlServer/Migrations/20200319151551_SeparateStatusFromInfo.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace OrganisationRegistry.SqlServer.Migrations
+{
+    public partial class SeparateStatusFromInfo : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "SyncInfo",
+                schema: "Magda",
+                table: "KboSyncQueue",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "SyncInfo",
+                schema: "Magda",
+                table: "KboSyncQueue");
+        }
+    }
+}
