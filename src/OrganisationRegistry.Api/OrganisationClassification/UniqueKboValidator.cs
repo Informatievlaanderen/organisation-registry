5591687f38bda066fd554d63378c45979642c9b6
Koen Metsu <koen@kometsoft.be>
2020-03-24 16:05:33 +0100

    fix: prevent from coupling to more than one organisation

--- src/OrganisationRegistry.Api/OrganisationClassification/UniqueKboValidator.cs
+++ src/OrganisationRegistry.Api/OrganisationClassification/UniqueKboValidator.cs
@@ -21,21 +21,12 @@ namespace OrganisationRegistry.Api.OrganisationClassification
         {
             var dotFormat = kboNumber.ToDotFormat();
             var digitsOnly = kboNumber.ToDigitsOnly();
-            var period = new Period(
-                new ValidFrom(messageValidFrom),
-                new ValidTo(messageValidTo));
 
             using (var context = _contextFactory().Value)
             {
-                var organisations =
-                    context.OrganisationDetail
-                        .Where(item => item.KboNumber == dotFormat || item.KboNumber == digitsOnly)
-                        .ToList();
-
-                return organisations.Any(item => period.OverlapsWith(
-                    new Period(
-                        new ValidFrom(item.ValidFrom),
-                        new ValidTo(item.ValidTo))));
+                return context
+                    .OrganisationDetail
+                    .Any(item => item.KboNumber == dotFormat || item.KboNumber == digitsOnly);
             }
         }
     }
