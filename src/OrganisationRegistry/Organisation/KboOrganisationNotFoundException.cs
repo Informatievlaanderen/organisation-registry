f150267a6c847222f6fab9d5f20a9f15a57b3271
Koen Metsu <koen@kometsoft.be>
2020-03-24 13:51:43 +0100

    feat: show kbo error message in exceptionz

--- src/OrganisationRegistry/Organisation/KboOrganisationNotFoundException.cs
+++ src/OrganisationRegistry/Organisation/KboOrganisationNotFoundException.cs
@@ -1,8 +1,10 @@
 namespace OrganisationRegistry.Organisation
 {
+    using System.Linq;
+
     public class KboOrganisationNotFoundException : DomainException
     {
-        public KboOrganisationNotFoundException()
-            : base("Organisatie werd niet gevonden in de VKBO.") { }
+        public KboOrganisationNotFoundException(string[] errorMessages)
+            : base($"Organisatie werd niet gevonden in de VKBO. Foutmeldingen:\n - {string.Join("\n -", errorMessages)}") { }
     }
 }
