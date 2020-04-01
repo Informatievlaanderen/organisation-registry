6b875f0fed845f5883b817b228d3d1e931d66b2f
Koen Metsu <koen@kometsoft.be>
2020-04-01 15:20:02 +0200

    fix: don't trim spaces when deserializing events

--- src/OrganisationRegistry.Infrastructure/EventStore/EventData.cs
+++ src/OrganisationRegistry.Infrastructure/EventStore/EventData.cs
@@ -1,6 +1,7 @@
 namespace OrganisationRegistry.Infrastructure.EventStore
 {
     using System;
+    using Newtonsoft.Json;
 
     public class EventData
     {
@@ -14,6 +15,26 @@ namespace OrganisationRegistry.Infrastructure.EventStore
         public string LastName { get; private set; }
         public string FirstName { get; private set; }
         public string UserId { get; private set; }
+
+        public EventData()
+        {
+
+        }
+
+        [JsonConstructor]
+        public EventData(Guid id, int number, int version, string name, DateTimeOffset timestamp, string data, string ip, string lastName, string firstName, string userId)
+        {
+            Id = id;
+            Number = number;
+            Version = version;
+            Name = name;
+            Timestamp = timestamp;
+            Data = data;
+            Ip = ip;
+            LastName = lastName;
+            FirstName = firstName;
+            UserId = userId;
+        }
 
         public EventData WithName(string name)
             => new EventData
