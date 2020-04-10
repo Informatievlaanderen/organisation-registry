5a746f2945ed0ba47637b8749c63bae7c413520b
Koen Metsu <koen@kometsoft.be>
2020-04-10 11:54:17 +0200

    feat: provide better exception for already coupled orgs

---
+++ test/OrganisationRegistry.UnitTests/Organisation/Kbo/CoupleAlreadyCoupledOrganisationToKboTests.cs
@@ -0,0 +1,170 @@
+namespace OrganisationRegistry.UnitTests.Organisation.Kbo
+{
+    using System;
+    using System.Collections.Generic;
+    using FluentAssertions;
+    using Infrastructure.Tests.Extensions.TestHelpers;
+    using KeyTypes.Events;
+    using LabelType.Events;
+    using Location;
+    using Location.Events;
+    using LocationType.Events;
+    using Microsoft.Extensions.Logging;
+    using Moq;
+    using OrganisationClassification;
+    using OrganisationClassification.Events;
+    using OrganisationClassificationType;
+    using OrganisationClassificationType.Events;
+    using OrganisationRegistry.Infrastructure.Events;
+    using OrganisationRegistry.Organisation;
+    using OrganisationRegistry.Organisation.Commands;
+    using OrganisationRegistry.Organisation.Events;
+    using Tests.Shared;
+    using Xunit;
+    using Xunit.Abstractions;
+    using Purpose = OrganisationRegistry.Organisation.Events.Purpose;
+
+    public class CoupleAlreadyCoupledOrganisationToKboTests: ExceptionSpecification<Organisation, KboOrganisationCommandHandlers, CoupleOrganisationToKbo>
+    {
+        private OrganisationRegistryConfigurationStub _organisationRegistryConfigurationStub;
+
+        private OrganisationId _organisationId;
+        private OrganisationClassificationTypeId _legalFormOrganisationClassificationTypeId;
+        private OrganisationClassificationId _organisationClassificationId;
+        private LocationId _registeredOfficeLocationId;
+        private readonly DateTime _kboOrganisationValidFromDate = new DateTime(2000, 12, 31);
+        private DateTimeProviderStub _dateTimeProviderStub;
+
+        protected override IEnumerable<IEvent> Given()
+        {
+            _organisationRegistryConfigurationStub = new OrganisationRegistryConfigurationStub
+            {
+                KboKeyTypeId = Guid.NewGuid(),
+                KboV2LegalFormOrganisationClassificationTypeId = Guid.NewGuid(),
+                KboV2RegisteredOfficeLocationTypeId = Guid.NewGuid(),
+                KboV2FormalNameLabelTypeId = Guid.NewGuid(),
+            };
+            _organisationId = new OrganisationId(Guid.NewGuid());
+            _registeredOfficeLocationId = new LocationId(Guid.NewGuid());
+            _legalFormOrganisationClassificationTypeId = new OrganisationClassificationTypeId(_organisationRegistryConfigurationStub.KboV2LegalFormOrganisationClassificationTypeId);
+            _organisationClassificationId = new OrganisationClassificationId(Guid.NewGuid());
+            _dateTimeProviderStub = new DateTimeProviderStub(DateTime.Today);
+
+            return new List<IEvent>
+            {
+                new OrganisationCreated(
+                    _organisationId,
+                    "organisation X",
+                    "OVO001234",
+                    "org",
+                    "",
+                    new List<Purpose>(),
+                    false,
+                    new ValidFrom(),
+                    new ValidTo()),
+
+                new KeyTypeCreated(
+                    _organisationRegistryConfigurationStub.KboKeyTypeId,
+                    "KBO sleutel"),
+
+                new OrganisationClassificationTypeCreated(
+                    _legalFormOrganisationClassificationTypeId,
+                    "ClassificatieType"),
+
+                new OrganisationClassificationCreated(
+                    _organisationClassificationId,
+                    "Classificatie",
+                    1,
+                    "Some Legal Code",
+                    true,
+                    _legalFormOrganisationClassificationTypeId,
+                    "ClassificatieType"),
+
+                new LocationTypeCreated(
+                    _organisationRegistryConfigurationStub.KboV2RegisteredOfficeLocationTypeId,
+                    "Registered KBO Office"),
+
+                new LocationCreated(_registeredOfficeLocationId,
+                    null,
+                    "Waregemsestraat, 8999 Evergem, Belgie",
+                    "Waregemsestraat",
+                    "8999",
+                    "Evergem",
+                    "Belgie"),
+
+                new LabelTypeCreated(
+                    _organisationRegistryConfigurationStub.KboV2FormalNameLabelTypeId,
+                    "Kbo formele naam"),
+
+                new OrganisationCoupledWithKbo(_organisationId, "BE0123456789", "organisation X", "OVO001234", _dateTimeProviderStub.Today)
+            };
+        }
+
+        protected override CoupleOrganisationToKbo When()
+        {
+            return new CoupleOrganisationToKbo(
+                _organisationId,
+                new KboNumber("BE0123456789"),
+                null);
+        }
+
+        protected override KboOrganisationCommandHandlers BuildHandler()
+        {
+            _dateTimeProviderStub = new DateTimeProviderStub(DateTime.Today);
+
+            return new KboOrganisationCommandHandlers(
+                new Mock<ILogger<KboOrganisationCommandHandlers>>().Object,
+                _organisationRegistryConfigurationStub,
+                Session,
+                new SequentialOvoNumberGenerator(),
+                new UniqueOvoNumberValidatorStub(false),
+                new UniqueKboNumberValidatorStub(true), // IMPORTANT => kbo number is already taken!
+                _dateTimeProviderStub,
+                new KboOrganisationRetrieverStub(new MockMagdaOrganisationResponse
+                {
+                    FormalName = new NameStub("NAME FROM KBO", _kboOrganisationValidFromDate),
+                    ShortName = new NameStub("SHORT NAME FROM KBO", _kboOrganisationValidFromDate),
+                    ValidFrom = _kboOrganisationValidFromDate,
+                    BankAccounts =
+                    {
+                        new BankAccountStub
+                        {
+                            AccountNumber = "BE71 0961 2345 6769",
+                            Bic = "GKCCBEBB",
+                            ValidFrom = new DateTime(2000, 1, 1),
+                            ValidTo = new DateTime(2001, 1, 1),
+                        }
+                    },
+                    LegalForm =
+                        new LegalFormStub
+                        {
+                            Code = "Some Legal Code",
+                            ValidFrom = new DateTime(2020, 12, 11),
+                            ValidTo = new DateTime(2020, 12, 12)
+                        },
+                    Address =
+                        new AddressStub
+                        {
+                            City = "Evergem",
+                            Street = "Zedelgemsestraat",
+                            Country = "Belgie",
+                            ZipCode = "9999",
+                            ValidFrom = new DateTime(2015, 5, 5),
+                            ValidTo = new DateTime(2016, 6, 6)
+                        }
+                }),
+                new KboOrganisationClassificationRetrieverStub("Some Legal Code", _organisationClassificationId),
+                new KboLocationRetrieverStub(address => address.Street == "Waregemsestraat" ? _registeredOfficeLocationId : (Guid?)null));
+        }
+
+        protected override int ExpectedNumberOfEvents => 0;
+
+        [Fact]
+        public void ThrowsOrganisationAlreadyCoupledWithKbo()
+        {
+            Exception.Should().BeOfType<OrganisationAlreadyCoupledWithKbo>();
+        }
+
+        public CoupleAlreadyCoupledOrganisationToKboTests(ITestOutputHelper helper) : base(helper) { }
+    }
+}
