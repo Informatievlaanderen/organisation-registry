6b7ba9e7d6f18fc8e64a24d056be83c66851e1b6
Koen Metsu <koen@kometsoft.be>
2020-04-27 15:13:05 +0200

    feat: add isEffective as column to seat type

---
+++ src/OrganisationRegistry.SqlServer/Migrations/20200427131048_AddIsEffectiveColumnToSeatType.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace OrganisationRegistry.SqlServer.Migrations
+{
+    public partial class AddIsEffectiveColumnToSeatType : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsEffective",
+                schema: "OrganisationRegistry",
+                table: "SeatTypeList",
+                nullable: false,
+                defaultValue: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsEffective",
+                schema: "OrganisationRegistry",
+                table: "SeatTypeList");
+        }
+    }
+}
