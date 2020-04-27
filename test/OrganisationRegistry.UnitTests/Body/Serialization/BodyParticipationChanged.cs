81abb713969d198cd693a67233b80ef8d463a889
Koen Metsu <koen@kometsoft.be>
2020-04-27 14:05:44 +0200

    test: add tests for deserialization and cmd behavior

---
+++ test/OrganisationRegistry.UnitTests/Body/Serialization/BodyParticipationChanged.cs
@@ -0,0 +1,34 @@
+namespace OrganisationRegistry.UnitTests.Body.Serialization
+{
+    using FluentAssertions;
+    using Newtonsoft.Json;
+    using OrganisationRegistry.Body.Events;
+    using Xunit;
+
+    public class BodyParticipationChanged
+    {
+        [Fact]
+        public void CanDeserializeWithTrue()
+        {
+            const string @event =
+                "{'bodyId':'9ca2e408-c1e0-113c-0e44-25db49cf6d1d','balancedParticipationObligatory':true,'balancedParticipationExtraRemark':'Aangeduid op 27/09/2019','balancedParticipationExceptionMeasure':null,'previousBalancedParticipationObligatory':false,'previousBalancedParticipationExtraRemark':null,'previousBalancedParticipationExceptionMeasure':null,'version':37,'timestamp':'2019-09-27T08:09:58Z'}";
+
+            var bodyBalancedParticipationChanged =
+                JsonConvert.DeserializeObject<BodyBalancedParticipationChanged>(@event);
+
+            bodyBalancedParticipationChanged.BalancedParticipationObligatory.Should().BeTrue();
+        }
+
+        [Fact]
+        public void CanDeserializeWithFalse()
+        {
+            const string @event =
+                "{'bodyId':'9ca2e408-c1e0-113c-0e44-25db49cf6d1d','balancedParticipationObligatory':false,'balancedParticipationExtraRemark':'Aangeduid op 27/09/2019','balancedParticipationExceptionMeasure':null,'previousBalancedParticipationObligatory':false,'previousBalancedParticipationExtraRemark':null,'previousBalancedParticipationExceptionMeasure':null,'version':37,'timestamp':'2019-09-27T08:09:58Z'}";
+
+            var bodyBalancedParticipationChanged =
+                JsonConvert.DeserializeObject<BodyBalancedParticipationChanged>(@event);
+
+            bodyBalancedParticipationChanged.BalancedParticipationObligatory.Should().BeFalse();
+        }
+    }
+}
---
+++ test/OrganisationRegistry.UnitTests/Body/WhenUpdatingBodyBalancedParticipation/WithNoParticipation.cs
@@ -0,0 +1,70 @@
+namespace OrganisationRegistry.UnitTests.Body.WhenUpdatingBodyBalancedParticipation
+{
+    using System;
+    using System.Collections.Generic;
+    using FluentAssertions;
+    using Infrastructure.Tests.Extensions.TestHelpers;
+    using Microsoft.Extensions.Logging;
+    using Moq;
+    using OrganisationRegistry.Body;
+    using OrganisationRegistry.Body.Commands;
+    using OrganisationRegistry.Body.Events;
+    using OrganisationRegistry.Infrastructure.Events;
+    using Tests.Shared;
+    using Xunit;
+    using Xunit.Abstractions;
+
+    public class WithNoParticipation : Specification<Body, BodyCommandHandlers, UpdateBodyBalancedParticipation>
+    {
+        private Guid _bodyId;
+
+        protected override BodyCommandHandlers BuildHandler()
+        {
+            return new BodyCommandHandlers(
+                new Mock<ILogger<BodyCommandHandlers>>().Object,
+                Session,
+                Mock.Of<IDateTimeProvider>(),
+                new SequentialBodyNumberGenerator(),
+                Mock.Of<IUniqueBodyNumberValidator>(),
+                Mock.Of<IBodySeatNumberGenerator>());
+        }
+
+        protected override IEnumerable<IEvent> Given()
+        {
+            _bodyId = Guid.NewGuid();
+
+            return new List<IEvent>
+            {
+                new BodyRegistered(_bodyId, "Body", "1", "bod", "some body", DateTime.Now, DateTime.Now),
+            };
+        }
+
+        protected override UpdateBodyBalancedParticipation When()
+        {
+            return new UpdateBodyBalancedParticipation(
+                new BodyId(_bodyId),
+                false,
+                "remark",
+                "exception");
+        }
+
+        protected override int ExpectedNumberOfEvents => 1;
+
+        [Fact]
+        public void SetsNoParticipation()
+        {
+            var bodyBalancedParticipationChanged = PublishedEvents[0].UnwrapBody<BodyBalancedParticipationChanged>();
+            bodyBalancedParticipationChanged.BodyId.Should().Be(_bodyId);
+
+            bodyBalancedParticipationChanged.BalancedParticipationObligatory.Should().BeFalse();
+            bodyBalancedParticipationChanged.BalancedParticipationExtraRemark.Should().Be("remark");
+            bodyBalancedParticipationChanged.BalancedParticipationExceptionMeasure.Should().Be("exception");
+
+            bodyBalancedParticipationChanged.PreviousBalancedParticipationObligatory.Should().BeNull();
+            bodyBalancedParticipationChanged.PreviousBalancedParticipationExtraRemark.Should().BeNull();
+            bodyBalancedParticipationChanged.PreviousBalancedParticipationExceptionMeasure.Should().BeNull();
+        }
+
+        public WithNoParticipation(ITestOutputHelper helper) : base(helper) { }
+    }
+}
