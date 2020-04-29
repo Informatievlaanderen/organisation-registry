3454071e8af83d6e171543e3a16510ab31ab7fe1
Koen Metsu <koen@metsu.be>
2020-04-29 11:41:20 +0200

    test: document existing bodyparticipation behaviour

---
+++ test/OrganisationRegistry.SqlServer.IntegrationTests/OnProjections/Reports/BodyParticipation/BodyParticipationReportTests.cs
@@ -0,0 +1,200 @@
+namespace OrganisationRegistry.SqlServer.IntegrationTests.OnProjections.Reports.BodyParticipation
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using Api.Infrastructure.Search.Filtering;
+    using Api.Infrastructure.Search.Sorting;
+    using Api.Report.Responses;
+    using FluentAssertions;
+    using Infrastructure;
+    using Microsoft.EntityFrameworkCore;
+    using OrganisationRegistry.Person;
+    using Reporting;
+    using Xunit;
+
+    public class BodyParticipationReportTests
+    {
+        private readonly SortingHeader _sorting;
+        private readonly FilteringHeader<BodyParticipationFilter> _filtering;
+        private readonly OrganisationRegistryContext _context;
+        private readonly Guid _bodyId;
+        private readonly Guid _bodySeatType1Id;
+        private readonly string _bodySeatType1Name;
+        private readonly Guid _bodySeatType2Id;
+        private readonly string _bodySeatType2Name;
+
+        public BodyParticipationReportTests()
+        {
+            _bodyId = Guid.NewGuid();
+            _bodySeatType1Id = Guid.NewGuid();
+            _bodySeatType1Name = "Effectief lid";
+            _bodySeatType2Id = Guid.NewGuid();
+            _bodySeatType2Name = "Voorzitter";
+
+            var dbContextOptions = new DbContextOptionsBuilder<OrganisationRegistryContext>()
+                .UseInMemoryDatabase(
+                    $"org-report-test-{Guid.NewGuid()}",
+                    builder => { }).Options;
+
+            _context = new OrganisationRegistryContext(dbContextOptions);
+
+            var bodySeatGenderRatioBodyItem = new BodySeatGenderRatioBodyItem
+            {
+                BodyId = _bodyId,
+                BodyName = "Ad-hoc adviescommissie impulssubsidies",
+                OrganisationId = Guid.Parse("6715d6f2-db97-427d-b093-293862670826"),
+                OrganisationName = "Literatuur Vlaanderen",
+                OrganisationIsActive = true,
+                LifecyclePhaseValidities = new List<BodySeatGenderRatioBodyLifecyclePhaseValidityItem>
+                {
+                    new BodySeatGenderRatioBodyLifecyclePhaseValidityItem
+                    {
+                        BodyId = _bodyId,
+                        RepresentsActivePhase = true,
+                        ValidFrom = null,
+                        ValidTo = null
+                    }
+                }
+            };
+            _context.BodySeatGenderRatioBodyList.Add(bodySeatGenderRatioBodyItem);
+
+            AddPost(_context, _bodyId, bodySeatGenderRatioBodyItem, _bodySeatType1Id, _bodySeatType1Name, Sex.Female);
+            AddPost(_context, _bodyId, bodySeatGenderRatioBodyItem, _bodySeatType1Id, _bodySeatType1Name, Sex.Female);
+            AddPost(_context, _bodyId, bodySeatGenderRatioBodyItem, _bodySeatType1Id, _bodySeatType1Name, Sex.Female);
+            AddPost(_context, _bodyId, bodySeatGenderRatioBodyItem, _bodySeatType1Id, _bodySeatType1Name, Sex.Male);
+            AddPost(_context, _bodyId, bodySeatGenderRatioBodyItem, _bodySeatType1Id, _bodySeatType1Name, Sex.Female);
+            AddPost(_context, _bodyId, bodySeatGenderRatioBodyItem, _bodySeatType2Id, _bodySeatType2Name, Sex.Female);
+
+            _context.SaveChanges();
+
+            _filtering = new FilteringHeader<BodyParticipationFilter>(new BodyParticipationFilter
+            {
+                EntitledToVote = true
+            });
+
+            _sorting = new SortingHeader(string.Empty, SortOrder.Ascending);
+        }
+
+        private static void AddPost(OrganisationRegistryContext context,
+            Guid bodyId,
+            BodySeatGenderRatioBodyItem bodySeatGenderRatioBodyItem,
+            Guid bodySeatTypeId,
+            string bodySeatTypeName,
+            Sex sex)
+        {
+
+            var bodySeatGenderRatioPostsPerTypeItem = new BodySeatGenderRatioPostsPerTypeItem
+            {
+                BodyId = bodyId,
+                BodySeatId = Guid.NewGuid(),
+                BodySeatTypeId = bodySeatTypeId,
+                BodySeatTypeName = bodySeatTypeName,
+                BodySeatValidFrom = null,
+                BodySeatValidTo = null,
+                EntitledToVote = true
+            };
+            bodySeatGenderRatioBodyItem.PostsPerType.Add(bodySeatGenderRatioPostsPerTypeItem);
+
+            context.BodySeatGenderRatioBodyMandateList.Add(
+                new BodySeatGenderRatioBodyMandateItem
+                {
+                    BodyId = bodySeatGenderRatioPostsPerTypeItem.BodyId,
+                    BodySeatTypeId = bodySeatGenderRatioPostsPerTypeItem.BodySeatTypeId,
+                    BodySeatId = bodySeatGenderRatioPostsPerTypeItem.BodySeatId,
+                    BodyMandateId = Guid.NewGuid(),
+                    BodyMandateValidFrom = null,
+                    BodyMandateValidTo = null,
+                    Assignments = new List<BodySeatGenderRatioAssignmentItem>
+                    {
+                        new BodySeatGenderRatioAssignmentItem
+                        {
+                            Id = Guid.NewGuid(),
+                            AssignmentValidFrom = null,
+                            AssignmentValidTo = null,
+                            PersonId = Guid.NewGuid(),
+                            DelegationAssignmentId = null,
+                            Sex = sex
+                        }
+                    }
+                });
+        }
+
+        [Fact]
+        public void DocumentExistingBodyParticipationBehaviour()
+        {
+            var participations =
+                BodyParticipation.Sort(
+                        BodyParticipation.Map(
+                            BodyParticipation.Search(
+                                _context,
+                                _bodyId,
+                                _filtering)),
+                        _sorting)
+                    .ToList();
+
+            participations.Should().BeEquivalentTo(
+                new List<BodyParticipation>
+                {
+                    new BodyParticipation
+                    {
+                        BodyId = _bodyId,
+                        AssignedCount = 5,
+                        BodyName = "Ad-hoc adviescommissie impulssubsidies",
+                        BodySeatTypeId = _bodySeatType1Id,
+                        BodySeatTypeName = _bodySeatType1Name,
+                        FemaleCount = 4,
+                        FemalePercentage = new decimal(0.8),
+                        MaleCount = 1,
+                        MalePercentage = new decimal(0.2),
+                        TotalCount = 5,
+                        UnassignedCount = 0,
+                        UnknownCount = 0,
+                        UnknownPercentage = 0,
+                    },
+                    new BodyParticipation
+                    {
+                        AssignedCount = 1,
+                        BodyId = _bodyId,
+                        BodyName = "Ad-hoc adviescommissie impulssubsidies",
+                        BodySeatTypeId = _bodySeatType2Id,
+                        BodySeatTypeName = _bodySeatType2Name,
+                        FemaleCount = 1,
+                        FemalePercentage = 1,
+                        MaleCount = 0,
+                        MalePercentage = 0,
+                        TotalCount = 1,
+                        UnassignedCount = 0,
+                        UnknownCount = 0,
+                        UnknownPercentage = 0,
+                    }
+                });
+        }
+
+        [Fact]
+        public void DocumentExistingBodyParticipationTotalsBehaviour()
+        {
+            var participationTotals =
+                BodyParticipationTotals.Map(
+                        BodyParticipationTotals.Search(
+                            _context,
+                            _bodyId,
+                            _filtering))
+                    .ToList();
+
+            participationTotals.Should().BeEquivalentTo(
+                new BodyParticipationTotals
+                {
+                    AssignedCount = 6,
+                    FemaleCount = 5,
+                    FemalePercentage = new decimal(0.83),
+                    MaleCount = 1,
+                    MalePercentage = new decimal(0.17),
+                    TotalCount = 6,
+                    UnassignedCount = 0,
+                    UnknownCount = 0,
+                    UnknownPercentage = 0,
+                });
+        }
+    }
+}
