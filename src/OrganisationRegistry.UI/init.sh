f5c390e2a2f16cfc40cde1077af784e8313953a6
Koen Metsu <koen@metsu.be>
2020-05-20 15:42:47 +0200

    fix: add window prop for aiv uri

--- src/OrganisationRegistry.UI/init.sh
+++ src/OrganisationRegistry.UI/init.sh
@@ -3,5 +3,6 @@
 echo "window.organisationRegistryVersion=\"v2-$API_VERSION\";" >> /usr/share/nginx/html/config.js
 echo "window.organisationRegistryApiEndpoint=\"$API_ENDPOINT\";" >> /usr/share/nginx/html/config.js
 echo "window.organisationRegistryUiEndpoint=\"$UI_ENDPOINT\";" >> /usr/share/nginx/html/config.js
+echo "window.organisationRegistryInformatieVlaanderenLink=\"$AIV_URI\";" >> /usr/share/nginx/html/config.js
 
 nginx -g 'daemon off;'
