6e1752c4fb329bf4195988864fa77cd849562af1
Koen Metsu <koen@metsu.be>
2020-05-29 10:53:28 +0200

    feat: show warning when total participation is nonCompliant

--- src/OrganisationRegistry.UI/app/services/reports/body-participation/body-participation-report-totals.model.ts
+++ src/OrganisationRegistry.UI/app/services/reports/body-participation/body-participation-report-totals.model.ts
@@ -1,13 +1,21 @@
+export class Compliance {
+  public static UNKNOWN: string = 'unknown';
+  public static NONCOMPLIANT: string = 'nonCompliant';
+  public static COMPLIANT: string = 'compliant';
+}
+
 export class BodyParticipationReportTotals {
-    constructor(
-      public maleCount: number = 0,
-      public femaleCount: number = 0,
-      public unknownCount: number = 0,
-      public assignedCount: number = 0,
-      public unassignedCount: number = 0,
-      public totalCount: number = 0,
-      public malePercentage: number = 0,
-      public femalePercentage: number = 0,
-      public unknownPercentage: number = 0,
-    ) { }
-  }  
+
+  constructor(
+    public maleCount: number = 0,
+    public femaleCount: number = 0,
+    public unknownCount: number = 0,
+    public assignedCount: number = 0,
+    public unassignedCount: number = 0,
+    public totalCount: number = 0,
+    public malePercentage: number = 0,
+    public femalePercentage: number = 0,
+    public unknownPercentage: number = 0,
+    public compliance: string = Compliance.UNKNOWN) {
+  }
+}
