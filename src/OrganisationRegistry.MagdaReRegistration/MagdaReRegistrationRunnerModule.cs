a971164522d5ccd5912b17e226bd7d73aab58730
Koen Metsu <koen@metsu.be>
2020-08-18 11:14:06 +0200

    feat: add application to reregister orgs with magda

---
+++ src/OrganisationRegistry.MagdaReRegistration/MagdaReRegistrationRunnerModule.cs
@@ -0,0 +1,50 @@
+namespace OrganisationRegistry.MagdaReRegistration
+{
+    using System;
+    using System.Reflection;
+    using Autofac;
+    using Autofac.Extensions.DependencyInjection;
+    using ElasticSearch;
+    using Infrastructure;
+    using Infrastructure.Events;
+    using Magda;
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.DependencyInjection;
+    using Microsoft.Extensions.Logging;
+    using SqlServer;
+
+    public class MagdaReRegistrationRunnerModule : Autofac.Module
+    {
+        private readonly IConfiguration _configuration;
+        private readonly IServiceCollection _services;
+        private readonly ILoggerFactory _loggerFactory;
+
+        public MagdaReRegistrationRunnerModule(
+            IConfiguration configuration,
+            IServiceCollection services,
+            ILoggerFactory loggerFactory)
+        {
+            _configuration = configuration;
+            _services = services;
+            _loggerFactory = loggerFactory;
+        }
+
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterModule(new InfrastructureModule(_configuration, ProvideScopedServiceProvider, _services));
+            builder.RegisterModule(new OrganisationRegistryModule());
+            builder.RegisterModule(new ElasticSearchModule(_configuration, _services));
+            builder.RegisterModule(new SqlServerModule(_configuration, _services, _loggerFactory));
+            builder.RegisterModule(new MagdaModule(_configuration));
+            builder.RegisterModule(new MagdaReRegistrationModule(_configuration, _services));
+
+            builder.Populate(_services);
+        }
+
+        private static Func<IServiceProvider> ProvideScopedServiceProvider(IComponentContext context)
+        {
+            var defaultServiceProvider = context.Resolve<IServiceProvider>();
+            return () => defaultServiceProvider.CreateScope().ServiceProvider;
+        }
+    }
+}
