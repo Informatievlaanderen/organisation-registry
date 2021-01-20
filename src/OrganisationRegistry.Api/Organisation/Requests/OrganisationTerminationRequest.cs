6600efdf3799739e09229abd667fd0448a843dbf
Koen Metsu <koen@metsu.be>
2021-01-20 14:00:28 +0100

    feat: add role restriction and date to terminate ctrl method

---
+++ src/OrganisationRegistry.Api/Organisation/Requests/OrganisationTerminationRequest.cs
@@ -0,0 +1,9 @@
+namespace OrganisationRegistry.Api.Organisation.Requests
+{
+    using System;
+
+    public class OrganisationTerminationRequest
+    {
+        public DateTime DateOfTermination { get; set; }
+    }
+}
