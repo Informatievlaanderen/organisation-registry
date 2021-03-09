84bbac8f5051bbf358d640592faa4cd267679aa5
David Cumps <david@cumps.be>
2021-03-09 13:02:14 +0100

    style: single quotes js

--- src/OrganisationRegistry.UI/app/organisation/buildings/overview/overview.component.ts
+++ src/OrganisationRegistry.UI/app/organisation/buildings/overview/overview.component.ts
@@ -14,7 +14,7 @@ import {
   OrganisationBuildingService,
   OrganisationBuildingFilter
 } from 'services/organisationbuildings';
-import {OrganisationInfoService} from "services";
+import { OrganisationInfoService } from 'services';
 
 @Component({
   templateUrl: 'overview.template.html',
