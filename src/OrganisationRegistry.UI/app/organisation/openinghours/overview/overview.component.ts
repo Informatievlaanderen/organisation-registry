6067c59d31522a87a7d8f03f45010118c7b66b9e
David Cumps <david@cumps.be>
2021-03-09 13:15:29 +0100

    style: space, quote

--- src/OrganisationRegistry.UI/app/organisation/openinghours/overview/overview.component.ts
+++ src/OrganisationRegistry.UI/app/organisation/openinghours/overview/overview.component.ts
@@ -14,7 +14,7 @@ import {
   OrganisationOpeningHourService,
   OrganisationOpeningHourFilter
 } from 'services/organisationopeninghours';
-import {OrganisationInfoService} from "services";
+import { OrganisationInfoService } from 'services';
 
 @Component({
   templateUrl: 'overview.template.html',
