437f7042e3bdd3a7ccc06ab2788e9bb7ce9633a0
Koen Metsu <koen@metsu.be>
2021-04-21 10:03:05 +0200

    feat: reduce async network io for memory caches
    Removes unnecessary fields being included in the select for building
    memory caches, thus reducing the network traffic to/from RDS.

--- src/OrganisationRegistry.SqlServer/Infrastructure/MemoryCacheExtensions.cs
+++ src/OrganisationRegistry.SqlServer/Infrastructure/MemoryCacheExtensions.cs
@@ -1,21 +1,10 @@
 ﻿namespace OrganisationRegistry.SqlServer.Infrastructure
 {
-    using Microsoft.EntityFrameworkCore;
     using System;
     using System.Collections.Generic;
-    using System.Linq;
-    using System.Threading.Tasks;
 
     public static class MemoryCacheExtensions
     {
-        public static Task<Dictionary<TKey, TValue>> BuildMemoryCache<T, TKey, TValue>(
-            this IQueryable<T> queryable,
-            Func<T, TKey> keySelector,
-            Func<T, TValue> valueSelector) where T : class
-        {
-            return queryable.AsNoTracking().ToDictionaryAsync(keySelector, valueSelector);
-        }
-
         public static void UpdateMemoryCache<T>(this Dictionary<Guid, T> dictionary, Guid key, T value)
         {
             if (dictionary.ContainsKey(key))
