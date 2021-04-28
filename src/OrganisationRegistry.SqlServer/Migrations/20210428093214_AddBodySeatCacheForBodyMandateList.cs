10bcf57b49de4fbb32b3df410e2410f2e2031a91
Koen Metsu <koen@metsu.be>
2021-04-28 16:40:18 +0200

    feat: add seat type name to body mandate view

---
+++ src/OrganisationRegistry.SqlServer/Migrations/20210428093214_AddBodySeatCacheForBodyMandateList.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace OrganisationRegistry.SqlServer.Migrations
+{
+    public partial class AddBodySeatCacheForBodyMandateList : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "BodySeatCacheForBodyMandateList",
+                schema: "OrganisationRegistry",
+                columns: table => new
+                {
+                    BodySeatId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    SeatTypeId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    SeatTypeName = table.Column<string>(type: "nvarchar(500)", maxLength: 500, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BodySeatCacheForBodyMandateList", x => x.BodySeatId)
+                        .Annotation("SqlServer:Clustered", false);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "BodySeatCacheForBodyMandateList",
+                schema: "OrganisationRegistry");
+        }
+    }
+}
