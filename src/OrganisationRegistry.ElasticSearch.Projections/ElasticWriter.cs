78bab9c52e00b5453030cac0e36f857b0d9c61c1
Koen Metsu <koen@metsu.be>
2021-06-02 11:44:43 +0200

    feat: extract scroll to separate method

---
+++ src/OrganisationRegistry.ElasticSearch.Projections/ElasticWriter.cs
@@ -0,0 +1,57 @@
+namespace OrganisationRegistry.ElasticSearch.Projections
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using Client;
+    using Nest;
+
+    public static class ElasticWriter
+    {
+        private static readonly TimeSpan ScrollTimeout = TimeSpan.FromMinutes(5);
+
+        public static async Task UpdateByScroll<T>(
+            Elastic elastic,
+            Func<QueryContainerDescriptor<T>, QueryContainer> query,
+            Func<T, Task> updateAction) where T : class
+        {
+            await elastic.WriteClient.Indices.RefreshAsync(Indices.Index<T>());
+
+            var searchResponse = await elastic.TryGetAsync(async () => (await elastic.WriteClient.SearchAsync<T>(
+                search => search
+                    .From(0)
+                    .Size(Elastic.MaxResultWindow)
+                    .Query(query)
+                    .Scroll(new Time(ScrollTimeout)))).ThrowOnFailure());
+
+            var documents = new List<T>();
+
+            while (searchResponse.Documents.Any())
+            {
+                foreach (var document in searchResponse.Documents.ToList())
+                {
+                    await updateAction(document);
+                }
+
+                documents.AddRange(searchResponse.Documents);
+
+                var response = searchResponse;
+                searchResponse = await elastic.TryGetAsync(async () => (await
+                    elastic.WriteClient.ScrollAsync<T>(new Time(ScrollTimeout),
+                        response.ScrollId)).ThrowOnFailure());
+
+            }
+
+            await elastic.TryAsync(async () => (await
+                    elastic.WriteClient.ClearScrollAsync(new ClearScrollRequest(searchResponse.ScrollId)))
+                .ThrowOnFailure());
+
+            if (!documents.Any())
+                return;
+
+            await elastic.TryAsync(async () =>
+                (await elastic.WriteClient.IndexManyAsync(documents)).ThrowOnFailure());
+        }
+    }
+}
