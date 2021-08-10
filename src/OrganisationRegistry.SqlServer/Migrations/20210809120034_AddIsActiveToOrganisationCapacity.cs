df09b0fae97a1f515ebb406fda2db57a38ee6d0a
Koen Metsu <koen@metsu.be>
2021-08-10 14:05:41 +0200

    feat: track IsActive in OrganisationCapacityListItemView

---
+++ src/OrganisationRegistry.SqlServer/Migrations/20210809120034_AddIsActiveToOrganisationCapacity.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace OrganisationRegistry.SqlServer.Migrations
+{
+    public partial class AddIsActiveToOrganisationCapacity : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsActive",
+                schema: "OrganisationRegistry",
+                table: "OrganisationCapacityList",
+                type: "bit",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsActive",
+                schema: "OrganisationRegistry",
+                table: "OrganisationCapacityList");
+        }
+    }
+}
