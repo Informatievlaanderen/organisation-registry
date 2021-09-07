ee70bedf6fb4aca5638439deea6f61e1971618cb
Koen Metsu <koen@metsu.be>
2021-09-07 15:03:25 +0200

    feat: show user a warning if on dev/staging

---
+++ src/OrganisationRegistry.UI/app/environments.ts
@@ -0,0 +1,5 @@
+export class Environments {
+  public static staging = 'staging';
+  public static development = 'development';
+  public static production = 'production';
+}
---
+++ src/OrganisationRegistry.UI/app/shared/components/environment-warning/environment-warning.component.ts
@@ -0,0 +1,21 @@
+﻿import {Component, OnInit, Input} from '@angular/core';
+
+import {Environments} from "../../../environments";
+
+@Component({
+  selector: 'ww-environment-warning',
+  styleUrls: ['./environment-warning.style.css'],
+  templateUrl: 'environment-warning.template.html'
+})
+export class EnvironmentWarningComponent implements OnInit {
+  @Input('environment') environment: string;
+
+  public showEnvironment: boolean;
+
+  constructor() {
+  }
+
+  ngOnInit() {
+    this.showEnvironment = this.environment != Environments.production;
+  }
+}
