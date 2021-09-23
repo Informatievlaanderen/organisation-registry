0c5b12c9f47504dee402c20f0d05d462d2551885
Koen Metsu <koen@metsu.be>
2021-09-23 09:29:30 +0200

    fix: ensure migrations history moved before migrating

---
+++ src/OrganisationRegistry.SqlServer/Migrations/20210915101141_MoveMigrationHistory.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace OrganisationRegistry.SqlServer.Migrations
+{
+    public partial class MoveMigrationHistory : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+        }
+    }
+}
