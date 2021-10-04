5ab8923e17ed2f84e33b132fe9c9dc33ad74da8a
Koen Metsu <koen@metsu.be>
2021-10-04 15:45:18 +0200

    feat: allow machine-to-machine auth

---
+++ src/OrganisationRegistry.Api/Edit/Organisation/OrganisationTestAuthController.cs
@@ -0,0 +1,33 @@
+﻿namespace OrganisationRegistry.Api.Edit.Organisation
+{
+    using System;
+    using System.Net;
+    using System.Threading.Tasks;
+    using Infrastructure;
+    using Microsoft.AspNetCore.Authorization;
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.FeatureManagement.Mvc;
+    using OrganisationRegistry.Infrastructure.Authorization;
+    using OrganisationRegistry.Infrastructure.Commands;
+    using Requests;
+    using AuthenticationSchemes = Infrastructure.Security.AuthenticationSchemes;
+
+    [FeatureGate(FeatureFlags.EditApi)]
+    [ApiVersion("1.0")]
+    [AdvertiseApiVersions("1.0")]
+    [OrganisationRegistryRoute("edit")]
+    public class OrganisationTestAuthController : OrganisationRegistryController
+    {
+        public OrganisationTestAuthController(ICommandSender commandSender)
+            : base(commandSender)
+        {
+        }
+
+        [HttpGet("auth")]
+        [Authorize(AuthenticationSchemes = AuthenticationSchemes.EditApi)]
+        public async Task<IActionResult> TestAuthorization()
+        {
+            return Ok();
+        }
+    }
+}
---
+++ src/OrganisationRegistry.Api/Infrastructure/Security/AuthenticationSchemes.cs
@@ -0,0 +1,7 @@
+namespace OrganisationRegistry.Api.Infrastructure.Security
+{
+    public static class AuthenticationSchemes
+    {
+        public const string EditApi = "EditApi";
+    }
+}
