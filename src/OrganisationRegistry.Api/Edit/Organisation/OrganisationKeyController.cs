f629997e220f4006df04fa5c166542b3da284068
Koen Metsu <koen@metsu.be>
2021-10-07 14:43:00 +0200

    feat: improve swagger docs

--- src/OrganisationRegistry.Api/Edit/Organisation/OrganisationKeyController.cs
+++ src/OrganisationRegistry.Api/Edit/Organisation/OrganisationKeyController.cs
@@ -1,20 +1,20 @@
 ﻿namespace OrganisationRegistry.Api.Edit.Organisation
 {
     using System;
-    using System.Collections.Generic;
-    using System.Net;
     using System.Threading.Tasks;
     using Be.Vlaanderen.Basisregisters.Api.Exceptions;
     using Infrastructure;
+    using Infrastructure.Swagger;
+    using Infrastructure.Swagger.Examples;
     using Microsoft.AspNetCore.Http;
     using Microsoft.AspNetCore.Mvc;
     using Microsoft.FeatureManagement.Mvc;
     using OrganisationRegistry.Infrastructure.Authorization;
     using OrganisationRegistry.Infrastructure.Commands;
     using Requests;
-    using Swashbuckle.AspNetCore.Annotations;
     using Swashbuckle.AspNetCore.Filters;
-    using EmptyResult = Infrastructure.EmptyResult;
+    using ForbiddenResponseExamples = Infrastructure.Swagger.Examples.ForbiddenResponseExamples;
+    using ProblemDetails = Be.Vlaanderen.Basisregisters.BasicApiProblem.ProblemDetails;
 
     [FeatureGate(FeatureFlags.EditApi)]
     [ApiVersion("1.0")]
@@ -22,6 +22,8 @@
     [OrganisationRegistryRoute("edit/organisations/{organisationId:guid}/keys")]
     [ApiController]
     [ApiExplorerSettings(GroupName = "Organisatiesleutels")]
+    [Consumes("application/json")]
+    [Produces("application/json")]
     public class OrganisationKeyController : OrganisationRegistryController
     {
         public OrganisationKeyController(ICommandSender commandSender)
@@ -29,20 +31,24 @@
         {
         }
 
-        /// <summary>Maak een organisatiesleutel aan.</summary>
+        /// <summary>Voeg een organisatiesleutel toe.</summary>
+        /// <remarks>Voegt een organisatiesleutel toe aan een organisatie.
+        /// <br />
+        /// Organisatiesleutels van hetzelfde type mogen niet overlappen in tijd.</remarks>
         /// <param name="organisationId">Id van de organisatie.</param>
         /// <response code="201">Als het verzoek aanvaard is.</response>
-        /// <response code="400">Als het verzoek ongeldige data bevat.</response>
+        /// <response code="400">Als het verzoek ongeldige data bevat.<example>a;sdlf</example></response>
+        /// <response code="403">Als u onvoldoende rechten heeft op dit sleuteltype.</response>
         /// <response code="500">Als er een interne fout is opgetreden.</response>
         [HttpPost]
-        [ProducesResponseType(StatusCodes.Status201Created)]
-        [ProducesResponseType(typeof(EmptyResult), StatusCodes.Status200OK)]
-        [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(typeof(object), StatusCodes.Status201Created)]
+        [ProducesResponseType(typeof(ValidationErrors), StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status403Forbidden)]
         [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status500InternalServerError)]
-        [SwaggerResponseExample(StatusCodes.Status400BadRequest, typeof(BadRequestResponseExamples))]
+        [SwaggerResponseExample(StatusCodes.Status201Created, typeof(EmptyResponseExamples))]
+        [SwaggerResponseExample(StatusCodes.Status403Forbidden, typeof(ForbiddenResponseExamples))]
         [SwaggerResponseExample(StatusCodes.Status500InternalServerError, typeof(InternalServerErrorResponseExamples))]
-        [Consumes("application/json")]
-        [Produces("application/json")]
+        [SwaggerLocationHeader]
         public async Task<IActionResult> Post(
             [FromServices] IEditSecurityService securityService,
             [FromRoute] Guid organisationId,
@@ -51,7 +57,7 @@
             var internalMessage = new AddOrganisationKeyInternalRequest(organisationId, message);
 
             if (!securityService.CanAddKey(message.KeyTypeId))
-                ModelState.AddModelError("NotAllowed", "U hebt niet voldoende rechten voor dit sleuteltype.");
+                return Forbid();
 
             if (!TryValidateModel(internalMessage))
                 return BadRequest(ModelState);
@@ -60,7 +66,9 @@
             addOrganisationKey.User = new User("Orafin", "Edit Api", "Orafin Edit Api", null, new[] { Role.Orafin });
             await CommandSender.Send(addOrganisationKey);
 
-            return Created(Url.Action(nameof(Backoffice.Organisation.OrganisationKeyController.Get), new { id = message.OrganisationKeyId }), null);
+            return Created(
+                Url.Action(nameof(Backoffice.Organisation.OrganisationKeyController.Get),
+                    new { id = message.OrganisationKeyId }), null);
         }
 
         /// <summary>Pas een organisatiesleutel aan.</summary>
@@ -69,11 +77,10 @@
         /// <response code="200">Indien de organisatiesleutel succesvol is aangepast.</response>
         /// <response code="400">Indien er validatiefouten zijn.</response>
         [HttpPut("{organisationKeyId:guid}")]
-        [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status200OK)]
-        [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(typeof(object), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(ValidationErrors), StatusCodes.Status400BadRequest)]
         [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status500InternalServerError)]
-        [SwaggerResponseExample(StatusCodes.Status200OK, typeof(BadRequestResponseExamples))]
-        [SwaggerResponseExample(StatusCodes.Status400BadRequest, typeof(BadRequestResponseExamples))]
+        [SwaggerResponseExample(StatusCodes.Status200OK, typeof(EmptyResponseExamples))]
         [SwaggerResponseExample(StatusCodes.Status500InternalServerError, typeof(InternalServerErrorResponseExamples))]
         public async Task<IActionResult> Put(
             [FromServices] IEditSecurityService securityService,
---
+++ src/OrganisationRegistry.Api/Infrastructure/Swagger/Examples/ValidationErrors.cs
@@ -0,0 +1,9 @@
+namespace OrganisationRegistry.Api.Infrastructure.Swagger.Examples
+{
+    using System.Collections.Generic;
+
+    public class ValidationErrors : Dictionary<string, string[]>
+    {
+
+    }
+}
