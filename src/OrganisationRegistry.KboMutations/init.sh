a1aef23a3b475213a7cbb0461a7b97535afd3f4a
Koen Metsu <koen@metsu.be>
2021-11-02 14:42:02 +0100

    feat: no longer use base64 for certs

--- src/OrganisationRegistry.KboMutations/init.sh
+++ src/OrganisationRegistry.KboMutations/init.sh
@@ -4,8 +4,8 @@ CONTAINERID=$(curl -s http://169.254.170.2/v2/metadata | jq -r ".Containers[] | 
 
 sed -i "s/REPLACE_CONTAINERID/$CONTAINERID/g" appsettings.json
 
-echo $CERT | base64 -d > ./cert.crt
-echo $CACERT | base64 -d >  ./cacert.crt
-echo $KEY | base64 -d > ./key.key
+echo $CERT > ./cert.crt
+echo $CACERT >  ./cacert.crt
+echo $KEY > ./key.key
 
 ./OrganisationRegistry.KboMutations
