fde963511ab30b10de8117601e9749da26fca7da
W0dan <W0dan.git@gmail.com>
2022-02-01 12:40:19 +0100

    feat: counter resets, added meter

--- src/OrganisationRegistry.ElasticSearch.Projections/Metrics/EnvelopeMetrics.cs
+++ src/OrganisationRegistry.ElasticSearch.Projections/Metrics/EnvelopeMetrics.cs
@@ -3,27 +3,42 @@
     using System.Collections.Generic;
     using App.Metrics;
     using App.Metrics.Counter;
+    using App.Metrics.Meter;
     using OrganisationRegistry.Infrastructure.Events;
 
     public class EnvelopeMetrics
     {
         private readonly IMetricsRoot _metrics;
+        private readonly MeterOptions _meter;
         private readonly CounterOptions _counter;
 
         public EnvelopeMetrics(IMetricsRoot metrics, string projectionName)
         {
             _metrics = metrics;
             _counter = GetCounter(projectionName);
+            _meter = GetMeter(projectionName);
         }
 
         private static CounterOptions GetCounter(string projectionName) =>
             new()
             {
-                Name = $"{projectionName}: Number of envelopes found",
+                Name = $"{projectionName}: [Counter] Number of envelopes",
                 MeasurementUnit = Unit.Calls,
+                ResetOnReporting = true,
             };
 
-        public void LogEnvelopeCount(IReadOnlyCollection<IEnvelope> envelopes) =>
+        private static MeterOptions GetMeter(string projectionName) =>
+            new()
+            {
+                Name = $"{projectionName}: [Meter] Number of envelopes",
+                MeasurementUnit = Unit.Calls,
+                RateUnit = TimeUnit.Minutes,
+            };
+
+        public void CountEnvelopes(IReadOnlyCollection<IEnvelope> envelopes) =>
             _metrics.Measure.Counter.Increment(_counter, envelopes.Count);
+
+        public void MeterEnvelopes(IReadOnlyCollection<IEnvelope> envelopes) =>
+            _metrics.Measure.Meter.Mark(_meter, envelopes.Count);
     }
 }
