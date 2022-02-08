aa119b4cd88a217ec21da799c32fec60a42bd848
Koen Metsu <koen@metsu.be>
2022-02-08 14:45:32 +0100

    fix: partial revert of too early committed code

--- src/OrganisationRegistry.Api/Infrastructure/OrganisationRegistryConfiguration.cs
+++ src/OrganisationRegistry.Api/Infrastructure/OrganisationRegistryConfiguration.cs
@@ -30,9 +30,6 @@ namespace OrganisationRegistry.Api.Infrastructure
         public Guid[] OrganisationClassificationTypeIdsToTerminateEndOfNextYear { get; }
         public Guid[] FormalFrameworkIdsToTerminateEndOfNextYear { get; }
 
-        public Guid[] FormalFrameworkIdsAllowedForVlimpers { get; }
-        public Guid[] FormalFrameworkIdsNotAllowedForOrganisationRegistryBeheerders { get; }
-
         public OrganisationRegistryConfiguration(
             ApiConfiguration configuration,
             OrganisationTerminationConfiguration? terminationConfiguration)
@@ -47,12 +44,6 @@ namespace OrganisationRegistry.Api.Infrastructure
 
             FormalFrameworkIdsToTerminateEndOfNextYear =
                 SplitGuids(terminationConfiguration?.FormalFrameworkIdsToTerminateEndOfNextYear);
-
-            FormalFrameworkIdsAllowedForVlimpers =
-                SplitGuids(_configuration.FormalFrameworkIdsAllowedForVlimpers);
-
-            FormalFrameworkIdsNotAllowedForOrganisationRegistryBeheerders =
-                SplitGuids(_configuration.FormalFrameworkIdsNotAllowedForOrganisatiebeheerders);
         }
 
         private static Guid[] SplitGuids(string? value)
---
+++ src/OrganisationRegistry/Organisation/IWegwijsConfiguration.cs
@@ -0,0 +1,28 @@
+namespace OrganisationRegistry.Organisation
+{
+    using System;
+
+    public interface IOrganisationRegistryConfiguration
+    {
+        Guid KboKeyTypeId { get; }
+
+        Guid KboLegalFormClassificationTypeId { get; }
+
+        [Obsolete("Use KboV2RegisteredOfficeLocationTypeId instead.")]
+        Guid RegisteredOfficeLocationTypeId { get; }
+
+        Guid KboV2FormalNameLabelTypeId { get; }
+
+        Guid KboV2RegisteredOfficeLocationTypeId { get; }
+
+        Guid KboV2LegalFormOrganisationClassificationTypeId { get; }
+        Guid[] OrganisationCapacityTypeIdsToTerminateEndOfNextYear { get; }
+        Guid[] OrganisationClassificationTypeIdsToTerminateEndOfNextYear { get; }
+        Guid[] FormalFrameworkIdsToTerminateEndOfNextYear { get; }
+        Guid OrafinKeyTypeId { get; }
+        Guid VlimpersKeyTypeId { get; }
+        string OrafinOvoCode { get; }
+        Guid FormalNameLabelTypeId { get; }
+        Guid FormalShortNameLabelTypeId { get; }
+    }
+}
