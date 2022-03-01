eaf71137a2ef1d937c9b0b9ca29479b3a84dbb77
Koen Metsu <koen@metsu.be>
2022-03-01 10:20:09 +0100

    fix: or-678 vlimpersbeheerder should not get beheerder role

---
+++ src/OrganisationRegistry.Api/Security/OrganisationRegistryApiClaims.cs
@@ -0,0 +1,12 @@
+namespace OrganisationRegistry.Api.Security
+{
+    public static class OrganisationRegistryApiClaims
+    {
+        public const string OrganisationRegistryBeheerder = "organisationRegistryBeheerder";
+        public const string VlimpersBeheerder = "vlimpersBeheerder";
+        public const string OrgaanBeheerder = "orgaanBeheerder";
+        public const string Invoerder = "organisatieBeheerder";
+        public const string Developer = "developer";
+        public const string AutomatedTask = "automatedTask";
+    }
+}
