0f958f4708414f1813d7e1ef46e61447d61b7c95
W0dan <W0dan.git@gmail.com>
2022-03-02 10:08:41 +0100

    feat: or-740 migration to fix nullable isActive column

---
+++ src/OrganisationRegistry.SqlServer/Migrations/20220302084933_AlterTable_OrganisationCapacityList_Set_IsActive_NotNull.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace OrganisationRegistry.SqlServer.Migrations
+{
+    public partial class AlterTable_OrganisationCapacityList_Set_IsActive_NotNull : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+UPDATE [Backoffice].[OrganisationCapacityList]
+SET IsActive = 0
+WHERE IsActive IS null
+");
+
+            migrationBuilder.AlterColumn<bool>(
+                name: "IsActive",
+                schema: "Backoffice",
+                table: "OrganisationCapacityList",
+                type: "bit",
+                nullable: false,
+                defaultValue: false,
+                oldClrType: typeof(bool),
+                oldType: "bit",
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<bool>(
+                name: "IsActive",
+                schema: "Backoffice",
+                table: "OrganisationCapacityList",
+                type: "bit",
+                nullable: true,
+                oldClrType: typeof(bool),
+                oldType: "bit");
+        }
+    }
+}
