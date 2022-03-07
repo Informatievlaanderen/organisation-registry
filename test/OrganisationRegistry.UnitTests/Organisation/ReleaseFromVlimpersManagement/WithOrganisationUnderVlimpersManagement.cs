f2a63c68b5fc70d7876d00b66f8b16aa35f16721
Koen Metsu <koen@metsu.be>
2022-03-07 10:23:09 +0100

    test: or-764 add tests

---
+++ test/OrganisationRegistry.UnitTests/Organisation/ReleaseFromVlimpersManagement/WithOrganisationUnderVlimpersManagement.cs
@@ -0,0 +1,86 @@
+namespace OrganisationRegistry.UnitTests.Organisation.ReleaseFromVlimpersManagement
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using AutoFixture;
+    using FluentAssertions;
+    using Infrastructure.Tests.Extensions.TestHelpers;
+    using Microsoft.Extensions.Logging;
+    using Moq;
+    using OrganisationRegistry.Infrastructure.Authorization;
+    using OrganisationRegistry.Infrastructure.Events;
+    using OrganisationRegistry.Organisation;
+    using OrganisationRegistry.Organisation.Commands;
+    using OrganisationRegistry.Organisation.Events;
+    using TerminateOrganisation;
+    using Tests.Shared;
+    using Tests.Shared.Stubs;
+    using Xunit;
+    using Xunit.Abstractions;
+
+    public class WithOrganisationUnderVlimpersManagement: Specification<Organisation, OrganisationCommandHandlers, ReleaseFromVlimpersManagement>
+    {
+        private OrganisationRegistryConfigurationStub _organisationRegistryConfigurationStub;
+
+        private OrganisationId _organisationId;
+        private DateTimeProviderStub _dateTimeProviderStub;
+
+        protected override IEnumerable<IEvent> Given()
+        {
+            var fixture = new Fixture();
+            fixture.CustomizeArticle();
+            fixture.CustomizePeriod();
+
+            _dateTimeProviderStub = new DateTimeProviderStub(DateTime.Today);
+            _organisationRegistryConfigurationStub = new OrganisationRegistryConfigurationStub();
+            _organisationId = new OrganisationId(Guid.NewGuid());
+
+            var validity = fixture.Create<Period>();
+            var formalValidity = fixture.Create<Period>();
+            return new List<IEvent>
+            {
+                new OrganisationCreated(
+                    _organisationId,
+                    fixture.Create<string>(),
+                    fixture.Create<string>(),
+                    fixture.Create<string>(),
+                    fixture.Create<Article>(),
+                    fixture.Create<string>(),
+                    fixture.Create<List<Purpose>>(),
+                    fixture.Create<bool>(),
+                    formalValidity.Start,
+                    formalValidity.End,
+                    validity.Start,
+                    validity.End),
+                new OrganisationPlacedUnderVlimpersManagement(_organisationId)
+            };
+        }
+
+        protected override ReleaseFromVlimpersManagement When()
+            => new ReleaseFromVlimpersManagement(
+                    _organisationId)
+                .WithUserRole(Role.OrganisationRegistryBeheerder);
+
+        protected override OrganisationCommandHandlers BuildHandler()
+            => new OrganisationCommandHandlers(
+                new Mock<ILogger<OrganisationCommandHandlers>>().Object,
+                Session,
+                new SequentialOvoNumberGenerator(),
+                new UniqueOvoNumberValidatorStub(false),
+                _dateTimeProviderStub,
+                _organisationRegistryConfigurationStub,
+                Mock.Of<ISecurityService>());
+
+
+        protected override int ExpectedNumberOfEvents => 1;
+
+        [Fact]
+        public void PlacesTheOrganisationUnderVlimpersManagement()
+        {
+            PublishedEvents.Single().Should().BeOfType<Envelope<OrganisationReleasedFromVlimpersManagement>>();
+        }
+
+        public WithOrganisationUnderVlimpersManagement(ITestOutputHelper helper) : base(helper) { }
+    }
+}
---
+++ test/OrganisationRegistry.UnitTests/SharedCustomizations.cs
@@ -0,0 +1,64 @@
+namespace OrganisationRegistry.UnitTests
+{
+    using System;
+    using AutoFixture;
+    using AutoFixture.Dsl;
+    using OrganisationRegistry.Organisation;
+
+    public static class SharedCustomizations
+    {
+        public static void CustomizeArticle(this IFixture fixture)
+        {
+            fixture.Customize<Article>(
+                composer =>
+                    composer.FromFactory<int>(value => Article.All[Math.Abs(value) % Article.All.Length]));
+        }
+
+        public static void CustomizeValidFrom(this IFixture fixture)
+        {
+            fixture.Customize<ValidFrom>(
+                composer =>
+                    composer.FromFactory(
+                        generator => generator.Next() % 2 == 0
+                            ? new ValidFrom(fixture.Create<DateTime?>())
+                            : new ValidFrom()));
+        }
+
+        public static void CustomizeValidTo(this IFixture fixture)
+        {
+            fixture.Customize<ValidTo>(
+                composer =>
+                    composer.FromFactory(
+                        generator => generator.Next() % 2 == 0
+                            ? new ValidTo(fixture.Create<DateTime?>())
+                            : new ValidTo()));
+        }
+
+        public static void CustomizePeriod(this IFixture fixture)
+        {
+            CustomizeValidFrom(fixture);
+            CustomizeValidTo(fixture);
+            fixture.Customize<Period>(
+                composer =>
+                    composer.FromFactory(
+                        generator =>
+                        {
+                            var validFrom = fixture.Create<ValidFrom>();
+                            var validTo = fixture.Create<ValidTo>();
+
+                            if (!validFrom.IsInfinite && !validTo.IsInfinite &&
+                                validTo.IsInPastOf(validFrom.DateTime!.Value))
+                                validTo = new ValidTo(validFrom.DateTime.Value.AddDays(fixture.Create<int>()));
+
+                            return new Period(validFrom, validTo);
+                        }));
+        }
+
+        private static IPostprocessComposer<T> FromFactory<T>(
+            this IFactoryComposer<T> composer,
+            Func<Random, T> factory)
+        {
+            return composer.FromFactory<int>(value => factory(new Random(value)));
+        }
+    }
+}
