1d6fedd1048193a5c73128a2bc7b3f1df8abf982
Koen Metsu <koen@metsu.be>
2022-03-22 09:48:24 +0100

    fix: or-751 use workRulesUrl instead of url

--- test/OrganisationRegistry.ElasticSearch.Tests/Scenario/OrganisationScenario.cs
+++ test/OrganisationRegistry.ElasticSearch.Tests/Scenario/OrganisationScenario.cs
@@ -1,6 +1,7 @@
 namespace OrganisationRegistry.ElasticSearch.Tests.Scenario
 {
     using System;
+    using Organisation.Events;
     using Projections.Organisations;
     using Specimen;
 
@@ -16,5 +17,34 @@ namespace OrganisationRegistry.ElasticSearch.Tests.Scenario
             AddCustomization(new ParameterNameArg<Guid>("functionId", functionTypeId));
             AddCustomization(new ParameterNameArg<Guid>("functionTypeId", functionTypeId));
         }
+
+        public OrganisationRegulationUpdated CreateOrganisationRegulationUpdated(OrganisationRegulationAdded organisationRegulationAdded)
+            => new(
+                organisationId: organisationRegulationAdded.OrganisationId,
+                organisationRegulationAdded.OrganisationRegulationId,
+                Create<Guid>(),
+                Create<string>(),
+                Create<Guid>(),
+                Create<string>(),
+                Create<string>(),
+                Create<string>(),
+                Create<string>(),
+                Create<DateTime?>(),
+                Create<string>(),
+                Create<string>(),
+                Create<DateTime?>(),
+                Create<DateTime?>(),
+                Create<Guid>(),
+                Create<string>(),
+                Create<Guid>(),
+                Create<string>(),
+                Create<string>(),
+                Create<DateTime?>(),
+                Create<DateTime?>(),
+                Create<string>(),
+                Create<string>(),
+                Create<DateTime?>(),
+                Create<string>(),
+                Create<string>());
     }
 }
