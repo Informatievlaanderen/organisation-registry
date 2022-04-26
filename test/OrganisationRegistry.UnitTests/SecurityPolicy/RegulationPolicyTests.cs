b7a2206822fcdf187788192b6903c7bf62342cb2
Quinten Van Assche <quinten.vanassche@vlaanderen.be>
2022-04-26 14:25:41 +0200

    feat: or-890 added unit tests

---
+++ test/OrganisationRegistry.UnitTests/SecurityPolicy/RegulationPolicyTests.cs
@@ -0,0 +1,56 @@
+﻿namespace OrganisationRegistry.UnitTests.SecurityPolicy;
+
+using AutoFixture;
+using FluentAssertions;
+using Handling.Authorization;
+using OrganisationRegistry.Infrastructure.Authorization;
+using OrganisationRegistry.Organisation.Exceptions;
+using Tests.Shared.Stubs;
+using Xunit;
+
+public class RegulationPolicyTests
+{
+    private readonly Fixture _fixture;
+
+    public RegulationPolicyTests()
+    {
+        _fixture = new Fixture();
+    }
+
+    public RegulationPolicy CreatePolicy()
+        => new();
+
+    [Theory]
+    [InlineData(Role.RegelgevingBeheerder)]
+    [InlineData(Role.AlgemeenBeheerder)]
+    public void RegelgevingDbBeheerderAndAdminIsAuthorized(Role role)
+    {
+        var user = new UserBuilder()
+            .AddRoles(role)
+            .Build();
+
+        var authorizationResult =
+            CreatePolicy()
+                .Check(user);
+
+        authorizationResult.Should().Be(AuthorizationResult.Success());
+    }
+
+    [Theory]
+    [InlineData(Role.DecentraalBeheerder)]
+    [InlineData(Role.VlimpersBeheerder)]
+    [InlineData(Role.Orafin)]
+    [InlineData(Role.OrgaanBeheerder)]
+    public void NonRegelgevingDbBeheerderIsNotAuthorized(Role role)
+    {
+        var user = new UserBuilder()
+            .AddRoles(role)
+            .Build();
+
+        var authorizationResult =
+            CreatePolicy()
+                .Check(user);
+
+        authorizationResult.ShouldFailWith<InsufficientRights>();
+    }
+}
