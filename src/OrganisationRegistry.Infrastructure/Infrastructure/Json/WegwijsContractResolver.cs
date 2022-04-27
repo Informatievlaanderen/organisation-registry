82a132e8572b70dad1e8764b82dfc8794d57773d
W0dan <W0dan.git@gmail.com>
2022-04-27 16:27:12 +0200

    feat: or-944 empty arrays and order of fields correct for search api

--- src/OrganisationRegistry.Infrastructure/Infrastructure/Json/WegwijsContractResolver.cs
+++ src/OrganisationRegistry.Infrastructure/Infrastructure/Json/WegwijsContractResolver.cs
@@ -1,5 +1,9 @@
 ﻿namespace OrganisationRegistry.Infrastructure.Infrastructure.Json
 {
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+    using System.Linq;
     using System.Reflection;
     using Newtonsoft.Json;
     using Newtonsoft.Json.Serialization;
@@ -10,12 +14,56 @@
 
         protected override JsonProperty CreateProperty(MemberInfo member, MemberSerialization memberSerialization)
         {
-            var prop = base.CreateProperty(member, memberSerialization);
+            var property = base.CreateProperty(member, memberSerialization);
 
-            if (prop.PropertyType == typeof(string) && SetStringDefaultValueToEmptyString)
-                prop.DefaultValue = "";
+            if (property.PropertyType is not { } propertyType)
+                return property;
+            if (property.PropertyName is not { } propertyName)
+                return property;
 
-            return prop;
+            property = HandleDefaultString(property, propertyType, SetStringDefaultValueToEmptyString);
+
+            property = HandleEmptyArray(property, propertyType, propertyName);
+
+            return property;
+        }
+
+        /// <summary>
+        /// Empty arrays are removed from the JSON
+        /// </summary>
+        /// <param name="property"></param>
+        /// <param name="propertyType"></param>
+        /// <param name="propertyName"></param>
+        /// <returns></returns>
+        private static JsonProperty HandleEmptyArray(JsonProperty property, Type propertyType, string propertyName)
+        {
+            if (propertyType == typeof(string))
+                return property;
+
+            if (propertyType.GetInterface(nameof(IEnumerable)) == null)
+                return property;
+
+            property.ShouldSerialize =
+                instance =>
+                {
+                    var propertyInfo = instance.GetType().GetProperties()
+                        .Single(p => p.Name.Equals(propertyName, StringComparison.InvariantCultureIgnoreCase));
+
+                    if (propertyInfo.GetValue(instance) as IEnumerable<object> is { } enumerable)
+                        return enumerable.Any();
+
+                    return false;
+                };
+
+            return property;
+        }
+
+        private static JsonProperty HandleDefaultString(JsonProperty property, Type propertyType, bool setStringDefaultValueToEmptyString)
+        {
+            if (propertyType == typeof(string) && setStringDefaultValueToEmptyString)
+                property.DefaultValue = "";
+
+            return property;
         }
     }
 }
