9e7bc2592ed2828342bf686f6ba7c1b8d1f43809
Koen Metsu <koen@metsu.be>
2022-04-28 14:41:23 +0200

    feat: add matomo tracking

--- src/OrganisationRegistry.UI/init.sh
+++ src/OrganisationRegistry.UI/init.sh
@@ -5,4 +5,8 @@ echo "window.organisationRegistryApiEndpoint=\"$API_ENDPOINT\";" >> /usr/share/n
 echo "window.organisationRegistryUiEndpoint=\"$UI_ENDPOINT\";" >> /usr/share/nginx/html/config.js
 echo "window.organisationRegistryInformatieVlaanderenLink=\"$AIV_URI\";" >> /usr/share/nginx/html/config.js
 
+sed -i 's/__MATOMO_SITE_ID__/'"$MATOMO_SITE_ID"'/' /usr/share/nginx/html/index.html
+sed -i 's/__MATOMO_URL__/'"$MATOMO_URL"'/' /usr/share/nginx/html/index.html
+sed -i 's/__MATOMO_CDN__/'"$MATOMO_CDN"'/' /usr/share/nginx/html/index.html
+
 nginx -g 'daemon off;'
