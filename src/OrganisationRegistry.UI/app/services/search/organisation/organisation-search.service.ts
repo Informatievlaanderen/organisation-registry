7dd2a7f7a99d4a62a7f94b38c02d25ed4a8d11f9
QuintenGreenstack <quinten.vanassche@greenstack.be>
2022-04-29 15:26:46 +0200

    fix: or-948 solve regression with sorting and paging

--- src/OrganisationRegistry.UI/app/services/search/organisation/organisation-search.service.ts
+++ src/OrganisationRegistry.UI/app/services/search/organisation/organisation-search.service.ts
@@ -18,12 +18,13 @@ export class OrganisationSearchService {
   constructor(
     private http: Http,
     private configurationService: ConfigurationService
-  ) { }
+  ) {
+  }
 
   public search(
     query: string,
-    sortBy:string,
-    sortOrder:SortOrder,
+    sortBy: string,
+    sortOrder: SortOrder,
     page: number = 1,
     pageSize: number = this.configurationService.defaultPageSize): Observable<PagedResult<OrganisationDocument>> {
 
@@ -31,13 +32,13 @@ export class OrganisationSearchService {
       .json()
       .build();
 
-    const offset = (page-1)*pageSize;
+    const offset = (page - 1) * pageSize;
     const limit = pageSize;
-    const sortOrderPrefix = sortOrder === SortOrder.Descending?"-":"";
+    const sortOrderPrefix = sortOrder === SortOrder.Descending ? "-" : "";
     const sort = `${sortOrderPrefix}${sortBy}`;
-
+    
     return this.http
-      .get(`${this.url}?q=${query}&offset=${offset}&limit=${limit}&sort=${sort}&fields=${this.fields}`, { headers: headers })
+      .get(`${this.url}?q=${query}&offset=${offset}&limit=${limit}&sort=${sort}&fields=${this.fields}`, {headers: headers})
       .map(this.toOrganisations);
   }
 
