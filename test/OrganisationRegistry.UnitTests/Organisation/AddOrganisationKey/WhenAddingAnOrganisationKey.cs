91cca65bd473441c4a068255d7aa49d556e4cf59
W0dan <W0dan.git@gmail.com>
2022-05-06 15:46:14 +0200

    feat: or-1027 refactor AddOrganisationKey

--- test/OrganisationRegistry.UnitTests/Organisation/AddOrganisationKey/WhenAddingAnOrganisationKey.cs
+++ test/OrganisationRegistry.UnitTests/Organisation/AddOrganisationKey/WhenAddingAnOrganisationKey.cs
@@ -11,49 +11,52 @@ namespace OrganisationRegistry.UnitTests.Organisation.AddOrganisationKey
     using Microsoft.Extensions.Logging;
     using Moq;
     using OrganisationRegistry.Infrastructure.Authorization;
-    using Tests.Shared;
     using OrganisationRegistry.Organisation;
-    using OrganisationRegistry.Organisation.Commands;
-
     using OrganisationRegistry.Organisation.Events;
     using Tests.Shared.Stubs;
     using Xunit;
     using Xunit.Abstractions;
 
-    public class WhenAddingAnOrganisationKey : OldSpecification<Organisation, OrganisationCommandHandlers, AddOrganisationKey>
+    public class WhenAddingAnOrganisationKey : Specification<AddOrganisationKeyCommandHandler, AddOrganisationKey>
     {
         private Guid _organisationId;
         private Guid _keyId;
         private Guid _organisationKeyId;
-        private string _value;
+        private const string Value = "12345ABC-@#$";
         private DateTime _validTo;
         private DateTime _validFrom;
 
-        protected override OrganisationCommandHandlers BuildHandler()
+        public WhenAddingAnOrganisationKey(ITestOutputHelper helper) : base(helper)
+        {
+        }
+
+        protected override AddOrganisationKeyCommandHandler BuildHandler()
         {
             var securityServiceMock = new Mock<ISecurityService>();
             securityServiceMock
-                .Setup(service =>
-                    service.CanUseKeyType(
-                        It.IsAny<IUser>(),
-                        It.IsAny<Guid>()))
+                .Setup(
+                    service =>
+                        service.CanUseKeyType(
+                            It.IsAny<IUser>(),
+                            It.IsAny<Guid>()))
                 .Returns(true);
 
-            return new OrganisationCommandHandlers(
-                new Mock<ILogger<OrganisationCommandHandlers>>().Object,
+            return new AddOrganisationKeyCommandHandler(
+                new Mock<ILogger<AddOrganisationKeyCommandHandler>>().Object,
                 Session,
-                new SequentialOvoNumberGenerator(),
-                null,
-                new DateTimeProvider(),
                 new OrganisationRegistryConfigurationStub(),
                 securityServiceMock.Object);
         }
+
+        protected override IUser User
+            => new UserBuilder()
+                .AddRoles(Role.AlgemeenBeheerder)
+                .Build();
 
         protected override IEnumerable<IEvent> Given()
         {
             _keyId = Guid.NewGuid();
             _organisationKeyId = Guid.NewGuid();
-            _value = "12345ABC-@#$";
             _validFrom = DateTime.Now.AddDays(1);
             _validTo = DateTime.Now.AddDays(2);
             _organisationId = Guid.NewGuid();
@@ -65,22 +68,16 @@ namespace OrganisationRegistry.UnitTests.Organisation.AddOrganisationKey
         }
 
         protected override AddOrganisationKey When()
-        {
-            return new AddOrganisationKey(
+            => new(
                 _organisationKeyId,
                 new OrganisationId(_organisationId),
                 new KeyTypeId(_keyId),
-                _value,
+                Value,
                 new ValidFrom(_validFrom),
-                new ValidTo(_validTo))
-            {
-                User = new UserBuilder()
-                    .AddRoles(Role.AlgemeenBeheerder)
-                    .Build()
-            };
-        }
+                new ValidTo(_validTo));
 
-        protected override int ExpectedNumberOfEvents => 1;
+        protected override int ExpectedNumberOfEvents
+            => 1;
 
         [Fact]
         public void AnOrganisationKeyAddedEventIsPublished()
@@ -94,11 +91,9 @@ namespace OrganisationRegistry.UnitTests.Organisation.AddOrganisationKey
             var organisationKeyAdded = PublishedEvents.First().UnwrapBody<OrganisationKeyAdded>();
             organisationKeyAdded.OrganisationId.Should().Be(_organisationId);
             organisationKeyAdded.KeyTypeId.Should().Be(_keyId);
-            organisationKeyAdded.Value.Should().Be(_value);
+            organisationKeyAdded.Value.Should().Be(Value);
             organisationKeyAdded.ValidFrom.Should().Be(_validFrom);
             organisationKeyAdded.ValidTo.Should().Be(_validTo);
         }
-
-        public WhenAddingAnOrganisationKey(ITestOutputHelper helper) : base(helper) { }
     }
 }
