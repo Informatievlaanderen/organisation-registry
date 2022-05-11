752db1670377e7f623867511e219e242bd1d9a7c
W0dan <W0dan.git@gmail.com>
2022-05-11 14:08:38 +0200

    feat: or-1016 remove organisation capacity backend

---
+++ src/OrganisationRegistry.Api/Backoffice/Organisation/Capacity/RemoveCapacityRequest.cs
@@ -0,0 +1,22 @@
+﻿namespace OrganisationRegistry.Api.Backoffice.Organisation.Capacity;
+
+using System;
+using OrganisationRegistry.Organisation;
+
+public class RemoveOrganisationCapacityRequest
+{
+    public Guid OrganisationId { get; set; }
+    public Guid OrganisationCapacityId { get; set; }
+
+    public RemoveOrganisationCapacityRequest(Guid organisationId, Guid organisationCapacityId)
+    {
+        OrganisationId = organisationId;
+        OrganisationCapacityId = organisationCapacityId;
+    }
+}
+
+public static class RemoveOrganisationCapacityRequestMapping
+{
+    public static RemoveOrganisationCapacity Map(RemoveOrganisationCapacityRequest message)
+        => new(new OrganisationId(message.OrganisationId), new OrganisationCapacityId(message.OrganisationCapacityId));
+}
