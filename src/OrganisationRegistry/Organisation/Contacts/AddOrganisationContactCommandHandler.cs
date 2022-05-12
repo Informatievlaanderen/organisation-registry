01bd01cbeaec5b1b4e8124dd07e850abd33b2d7a
QuintenGreenstack <quinten.vanassche@greenstack.be>
2022-05-12 13:28:59 +0200

    refactor: or-979 AddOrganisationContact

---
+++ src/OrganisationRegistry/Organisation/Contacts/AddOrganisationContactCommandHandler.cs
@@ -0,0 +1,38 @@
+﻿namespace OrganisationRegistry.Organisation.Contacts;
+
+using System.Threading.Tasks;
+using Commands;
+using ContactType;
+using Handling;
+using Infrastructure.Authorization;
+using Infrastructure.Commands;
+using Infrastructure.Domain;
+using Microsoft.Extensions.Logging;
+
+public class AddOrganisationContactCommandHandler :
+    BaseCommandHandler<AddOrganisationContactCommandHandler>,
+    ICommandEnvelopeHandler<AddOrganisationContact>
+{
+    public AddOrganisationContactCommandHandler(
+        ILogger<AddOrganisationContactCommandHandler> logger,
+        ISession session) : base(logger, session)
+    {
+    }
+
+    public Task Handle(ICommandEnvelope<AddOrganisationContact> envelope)
+        => UpdateHandler<Organisation>.For(envelope.Command, envelope.User, Session)
+            .Handle(
+                session =>
+                {
+                    var organisation = session.Get<Organisation>(envelope.Command.OrganisationId);
+                    organisation.ThrowIfTerminated(envelope.User);
+
+                    var contactType = session.Get<ContactType>(envelope.Command.ContactTypeId);
+
+                    organisation.AddContact(
+                        envelope.Command.OrganisationContactId,
+                        contactType,
+                        envelope.Command.ContactValue,
+                        new Period(new ValidFrom(envelope.Command.ValidFrom), new ValidTo(envelope.Command.ValidTo)));
+                });
+}
