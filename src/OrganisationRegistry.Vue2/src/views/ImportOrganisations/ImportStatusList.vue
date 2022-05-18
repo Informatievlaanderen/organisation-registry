94004f516424b4b0da9f258935474e4933894f59
W0dan <W0dan.git@gmail.com>
2022-05-18 11:15:47 +0200

    feat: or-1013 implement statuslist

---
+++ src/OrganisationRegistry.Vue2/src/views/ImportOrganisations/ImportStatusList.vue
@@ -0,0 +1,41 @@
+﻿<template>
+  <div class="vl-u-table-overflow">
+    <vl-data-table>
+      <caption>
+        Import status
+      </caption>
+      <thead>
+        <tr>
+          <th>Bestandsnaam</th>
+          <th>Status</th>
+          <th>Resultaat</th>
+          <th>Geïmporteerd op</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr :key="importStatus.id" v-for="importStatus in importStatuses">
+          <td>{{ importStatus.fileName }}</td>
+          <td>{{ importStatus.status }}</td>
+          <td>Resultaat</td>
+          <td>{{ importStatus.uploadedAt }}</td>
+        </tr>
+      </tbody>
+      <tfoot>
+        <tr>
+          <td><vl-annotation>Table annotation</vl-annotation></td>
+        </tr>
+      </tfoot>
+    </vl-data-table>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ImportStatusList",
+  props: {
+    importStatuses: Array,
+  },
+};
+</script>
+
+<style scoped></style>
