fea7093a197fd237e5d9c3cf794bb8130ab8cd42
QuintenGreenstack <quinten.vanassche@greenstack.be>
2022-05-18 11:54:54 +0200

    feat: or-1013 added apiUri to vue

--- src/OrganisationRegistry.Vue2/src/main.js
+++ src/OrganisationRegistry.Vue2/src/main.js
@@ -6,6 +6,10 @@ import "./components";
 
 Vue.config.productionTip = false;
 
+window.organisatieRegisterApiEndpoint =
+  window.organisatieRegisterApiEndpoint ||
+  "https://api.organisatie.dev-vlaanderen.local:9003";
+
 new Vue({
   router,
   render: (h) => h(App),
