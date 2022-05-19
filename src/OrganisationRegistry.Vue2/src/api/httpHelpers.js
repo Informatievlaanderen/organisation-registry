84939c1bf4e56693519a25c88e72027d7b160bd3
Koen Metsu <koen@metsu.be>
2022-05-19 13:21:08 +0200

    fix: or-1013 fix or api uri param name

--- src/OrganisationRegistry.Vue2/src/api/httpHelpers.js
+++ src/OrganisationRegistry.Vue2/src/api/httpHelpers.js
@@ -4,7 +4,7 @@
  * @returns {string} - The API uri
  */
 export function composeApiUri(route) {
-  return `${window.organisatieRegisterApiEndpoint}/v1/${route}`;
+  return `${window.organisationRegistryApiEndpoint}/v1/${route}`;
 }
 
 /**
