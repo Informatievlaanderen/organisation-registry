5d2adbe6ef8bdefd705a9fa6e8b54377f76a7b11
QuintenGreenstack <quinten.vanassche@greenstack.be>
2022-05-20 16:03:09 +0200

    refactor: or-979 AddOrganisationBuilding

---
+++ test/OrganisationRegistry.Tests.Shared/EquivalencyAssertionOptionsExtentions.cs
@@ -0,0 +1,11 @@
+﻿namespace OrganisationRegistry.UnitTests;
+
+using FluentAssertions.Equivalency;
+using OrganisationRegistry.Infrastructure.Events;
+
+public static class EquivalencyAssertionOptionsExtentions
+{
+    public static EquivalencyAssertionOptions<TEvent> IgnoreEventProperties<TEvent>(
+        this EquivalencyAssertionOptions<TEvent> options) where TEvent : IEvent
+        => options.Excluding(e => e.Timestamp).Excluding(e => e.Version);
+}
