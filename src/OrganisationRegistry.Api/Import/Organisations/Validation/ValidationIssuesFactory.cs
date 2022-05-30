b53020b1054ce3e8fdf434c92863fdfc26a55903
QuintenGreenstack <quinten.vanassche@greenstack.be>
2022-05-30 16:25:16 +0200

    fix: or-1025 add quotes to column names in error messages

--- src/OrganisationRegistry.Api/Import/Organisations/Validation/ValidationIssuesFactory.cs
+++ src/OrganisationRegistry.Api/Import/Organisations/Validation/ValidationIssuesFactory.cs
@@ -8,7 +8,7 @@ public static class ValidationIssuesFactory
 {
     public static ValidationIssue? Create(ImmutableList<string> issues, Func<string, string> formatValidationMessage)
         => issues.Any()
-            ? new ValidationIssue(formatValidationMessage(string.Join(", ", issues)))
+            ? new ValidationIssue(formatValidationMessage(string.Join(", ", issues.Select(i => $"'{i}'"))))
             : null;
 
     public static ValidationIssue? Create(string? maybeIssue, Func<string, string> formatValidationMessage)
