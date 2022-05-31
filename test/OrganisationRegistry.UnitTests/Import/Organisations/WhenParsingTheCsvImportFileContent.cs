e56f14a0e206fb342cff62ef6f7d57fdf2556b0d
QuintenGreenstack <quinten.vanassche@greenstack.be>
2022-05-31 10:30:15 +0200

    feat: or-1039 validate required fields

--- test/OrganisationRegistry.UnitTests/Import/Organisations/WhenParsingTheCsvImportFileContent.cs
+++ test/OrganisationRegistry.UnitTests/Import/Organisations/WhenParsingTheCsvImportFileContent.cs
@@ -8,7 +8,7 @@ using Xunit;
 
 public class WhenParsingTheCsvImportFileContent
 {
-    private static List<ImportRecord> Parse(string csvToParse)
+    private static List<OutputRecord> Parse(string csvToParse)
         => ImportFileParser.Parse(csvToParse).ToList();
 
     [Fact]
@@ -17,13 +17,15 @@ public class WhenParsingTheCsvImportFileContent
         // Arrange
         const string csvToParse = "reference;parent;name\n" +
                                   "REF1; Ovo00025; name1\n" +
-                                  "REF2; Ovo00026; name2";
+                                  "REF2; Ovo00026; name2\n" +
+                                  "REF3; ; name3";
         // Act
         var importedRecords = Parse(csvToParse);
         // Assert
-        importedRecords.Should().HaveCount(2);
-        importedRecords[0].Should().BeEquivalentTo(new ImportRecord("REF1", "name1", "Ovo00025"));
-        importedRecords[1].Should().BeEquivalentTo(new ImportRecord("REF2", "name2", "Ovo00026"));
+        importedRecords.Should().HaveCount(3);
+        importedRecords[0].Should().BeEquivalentTo(new OutputRecord("REF1", "name1", "Ovo00025"));
+        importedRecords[1].Should().BeEquivalentTo(new OutputRecord("REF2", "name2", "Ovo00026"));
+        importedRecords[2].Should().BeEquivalentTo(new OutputRecord("REF3", "name3", ""));
     }
 
     [Fact]
@@ -37,8 +39,8 @@ public class WhenParsingTheCsvImportFileContent
         var importedRecords = Parse(csvToParse);
         // Assert
         importedRecords.Should().HaveCount(2);
-        importedRecords[0].Should().BeEquivalentTo(new ImportRecord("REF1", "name1", "Ovo00025"));
-        importedRecords[1].Should().BeEquivalentTo(new ImportRecord("REF2", "name2", "Ovo00026"));
+        importedRecords[0].Should().BeEquivalentTo(new OutputRecord("REF1", "name1", "Ovo00025"));
+        importedRecords[1].Should().BeEquivalentTo(new OutputRecord("REF2", "name2", "Ovo00026"));
     }
 
     [Fact]
@@ -53,6 +55,40 @@ public class WhenParsingTheCsvImportFileContent
         importedRecords[0].Errors[0].Should().Be("Rij heeft incorrect aantal kollomen.");
         importedRecords[1].Errors.Should().HaveCount(1);
         importedRecords[1].Errors[0].Should().Be("Rij heeft incorrect aantal kollomen.");
+    }
+
+    [Fact]
+    public void ItTrimsTheFieldValues()
+    {
+        const string csvToParse = "reference;parent;name\n" +
+                                  "REF1   ;    Ovo00025;  name1  \n" +
+                                  "  REF2  ; Ovo00026   ;   name2  ";
+        // Act
+        var importedRecords = Parse(csvToParse);
+        // Assert
+        importedRecords.Should().HaveCount(2);
+        importedRecords[0].Should().BeEquivalentTo(new OutputRecord("REF1", "name1", "Ovo00025"));
+        importedRecords[1].Should().BeEquivalentTo(new OutputRecord("REF2", "name2", "Ovo00026"));
+    }
+
+    [Fact]
+    public void ItAddsFieldValueRequired()
+    {
+        // Arrange
+        const string csvToParse = "reference;parent;name\n" +
+                                  "; Ovo00025; name1\n" +
+                                  "REF2; Ovo00026;\n" +
+                                  ";Ovo00027;";
+        // Act
+        var importedRecords = Parse(csvToParse);
+        // Assert
+        importedRecords.Should().HaveCount(3);
+        importedRecords[0].Errors.Should().HaveCount(1);
+        importedRecords[0].Errors[0].Should().Be("Rij ontbreekt waarde voor volgende kolommen: 'reference'.");
+        importedRecords[1].Errors.Should().HaveCount(1);
+        importedRecords[1].Errors[0].Should().Be("Rij ontbreekt waarde voor volgende kolommen: 'name'.");
+        importedRecords[2].Errors.Should().HaveCount(1);
+        importedRecords[2].Errors[0].Should().Be("Rij ontbreekt waarde voor volgende kolommen: 'reference', 'name'.");
     }
 
 
