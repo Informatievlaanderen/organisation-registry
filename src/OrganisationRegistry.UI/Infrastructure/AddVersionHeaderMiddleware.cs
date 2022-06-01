981e97831be8e66aba374700e5a02181ca211fa2
W0dan <W0dan.git@gmail.com>
2022-06-01 15:00:34 +0200

    feat: or-1071 refactor namespaces ui

--- src/OrganisationRegistry.UI/Infrastructure/AddVersionHeaderMiddleware.cs
+++ src/OrganisationRegistry.UI/Infrastructure/AddVersionHeaderMiddleware.cs
@@ -1,24 +1,23 @@
-﻿namespace OrganisationRegistry.UI.Infrastructure
+﻿namespace OrganisationRegistry.UI.Infrastructure;
+
+using System.Reflection;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+public class AddVersionHeaderMiddleware
 {
-    using System.Reflection;
-    using System.Threading.Tasks;
-    using Microsoft.AspNetCore.Http;
+    private readonly RequestDelegate _next;
 
-    public class AddVersionHeaderMiddleware
+    public AddVersionHeaderMiddleware(RequestDelegate next)
     {
-        private readonly RequestDelegate _next;
+        _next = next;
+    }
 
-        public AddVersionHeaderMiddleware(RequestDelegate next)
-        {
-            _next = next;
-        }
+    public Task Invoke(HttpContext context)
+    {
+        var version = Assembly.GetEntryAssembly()!.GetName().Version!.ToString();
+        context.Response.Headers.Add("x-wegwijs-version", version);
 
-        public Task Invoke(HttpContext context)
-        {
-            var version = Assembly.GetEntryAssembly()!.GetName().Version!.ToString();
-            context.Response.Headers.Add("x-wegwijs-version", version);
-
-            return _next(context);
-        }
+        return _next(context);
     }
 }
