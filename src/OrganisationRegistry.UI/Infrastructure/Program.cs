f77ac98ce619f3ed690a2b58d41a19d8609c7cdb
W0dan <W0dan.git@gmail.com>
2022-06-01 15:00:34 +0200

    feat: or-1071 refactor namespaces ui

--- src/OrganisationRegistry.UI/Infrastructure/Program.cs
+++ src/OrganisationRegistry.UI/Infrastructure/Program.cs
@@ -1,44 +1,43 @@
-namespace OrganisationRegistry.UI.Infrastructure
+namespace OrganisationRegistry.UI.Infrastructure;
+
+using System.IO;
+using System.Net;
+using System.Security.Cryptography.X509Certificates;
+using Microsoft.AspNetCore.Hosting;
+
+public class Program
 {
-    using System.IO;
-    using System.Net;
-    using System.Security.Cryptography.X509Certificates;
-    using Microsoft.AspNetCore.Hosting;
-
-    public class Program
+    public static void Main(string[] args)
     {
-        public static void Main(string[] args)
+        IWebHostBuilder hostBuilder = new WebHostBuilder();
+        var environment = hostBuilder.GetSetting("environment");
+
+        if (environment == "Development")
         {
-            IWebHostBuilder hostBuilder = new WebHostBuilder();
-            var environment = hostBuilder.GetSetting("environment");
+            var cert = new X509Certificate2("organisationregistry-ui.pfx", "organisationregistry");
 
-            if (environment == "Development")
-            {
-                var cert = new X509Certificate2("organisationregistry-ui.pfx", "organisationregistry");
-
-                hostBuilder = hostBuilder
-                    .UseKestrel(server =>
+            hostBuilder = hostBuilder
+                .UseKestrel(server =>
+                {
+                    server.AddServerHeader = false;
+                    server.Listen(IPAddress.Any, 2443, listenOptions =>
                     {
-                        server.AddServerHeader = false;
-                        server.Listen(IPAddress.Any, 2443, listenOptions =>
-                        {
-                            listenOptions.UseHttps(cert);
-                        });
-                    })
-                    .UseUrls("https://organisatie.dev-vlaanderen.local:1443");
-            }
-            else
-            {
-                hostBuilder = hostBuilder.UseKestrel(server => server.AddServerHeader = false);
-            }
-
-            var host = hostBuilder
-                .UseContentRoot(Directory.GetCurrentDirectory())
-                //.UseIISIntegration()
-                .UseStartup<Startup>()
-                .Build();
-
-            host.Run();
+                        listenOptions.UseHttps(cert);
+                    });
+                })
+                .UseUrls("https://organisatie.dev-vlaanderen.local:1443");
         }
+        else
+        {
+            hostBuilder = hostBuilder.UseKestrel(server => server.AddServerHeader = false);
+        }
+
+        var host = hostBuilder
+            .UseContentRoot(Directory.GetCurrentDirectory())
+            //.UseIISIntegration()
+            .UseStartup<Startup>()
+            .Build();
+
+        host.Run();
     }
 }
