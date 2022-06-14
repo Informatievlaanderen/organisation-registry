62b15fd86701874332085ada110c1d996ee4208b
W0dan <W0dan.git@gmail.com>
2022-06-14 14:54:38 +0200

    feat: or-1042 return better fault when parent ref is wrong

---
+++ src/OrganisationRegistry/Organisation/Create/CircularDependencyOrFaultyReferenceDiscoveredBetweenOrganisations.cs
@@ -0,0 +1,17 @@
+﻿namespace OrganisationRegistry.Organisation;
+
+using System;
+using System.Collections.Immutable;
+using System.Linq;
+
+public class CircularDependencyOrFaultyReferenceDiscoveredBetweenOrganisations : Exception
+{
+    public CircularDependencyOrFaultyReferenceDiscoveredBetweenOrganisations(ImmutableArray<(int linenumber, string reference)> faultyReferences)
+        : base(ComposeMessage(faultyReferences))
+    {
+    }
+
+    private static string ComposeMessage(ImmutableArray<(int linenumber, string reference)> faultyReferences)
+        => Enumerable.Aggregate(faultyReferences, "Circulaire afhankelijkheden tussen organisaties of foutieve moeder-referentie(s) ontdekt:\n",
+            (current, reference) => current + $"'{reference.reference}' op lijn {reference.linenumber}\n");
+}
