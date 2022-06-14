9e30ed6f90bf52229309b035b9bab9a109ce34f2
QuintenGreenstack <quinten.vanassche@greenstack.be>
2022-06-14 14:20:35 +0200

    feat: or-1069 avoid unknow charachters in csv files

--- src/OrganisationRegistry.Api/Import/Organisations/Validation/ImportOrganisationCsvHeaderValidator.cs
+++ src/OrganisationRegistry.Api/Import/Organisations/Validation/ImportOrganisationCsvHeaderValidator.cs
@@ -11,8 +11,14 @@ using Microsoft.Extensions.Logging;
 
 public static class ImportOrganisationCsvHeaderValidator
 {
-    private static readonly ImmutableList<string> RequiredColumnNames = ImmutableList.Create<string>("reference", "parent", "name");
-    private static readonly ImmutableList<string> OptionalColumnNames = ImmutableList.Create<string>("validity_start", "shortname", "article", "operationalvalidity_start");
+    private static readonly ImmutableList<string> RequiredColumnNames =
+        ImmutableList.Create<string>("reference", "parent", "name");
+
+    private static readonly ImmutableList<string> OptionalColumnNames = ImmutableList.Create<string>(
+        "validity_start",
+        "shortname",
+        "article",
+        "operationalvalidity_start");
 
     public static CsvValidationResult Validate(ILogger logger, string filename, string csvContent)
     {
@@ -20,7 +26,9 @@ public static class ImportOrganisationCsvHeaderValidator
         try
         {
             using var reader = new StringReader(csvContent);
-            using var csv = new CsvReader(reader, new CsvConfiguration(CultureInfo.InvariantCulture) { Delimiter = ";" });
+            using var csv = new CsvReader(
+                reader,
+                new CsvConfiguration(CultureInfo.InvariantCulture) { Delimiter = ";" });
 
             csv.Read();
             csv.ReadHeader();
@@ -29,17 +37,28 @@ public static class ImportOrganisationCsvHeaderValidator
                 .Select(columnName => columnName.Trim().ToLower())
                 .ToImmutableList();
 
-            validationIssues = validationIssues.Add(InvalidFilename.Validate(filename));
-            validationIssues = validationIssues.Add(MissingRequiredColumns.Validate(csvHeaderRecord, RequiredColumnNames));
-            validationIssues = validationIssues.Add(DuplicateColumns.Validate(csvHeaderRecord));
-            validationIssues = validationIssues.Add(InvalidColumns.Validate(csvHeaderRecord, RequiredColumnNames.AddRange(OptionalColumnNames)));
+            validationIssues = validationIssues
+                .Add(InvalidFilename.Validate(filename))
+                .Add(MissingRequiredColumns.Validate(csvHeaderRecord, RequiredColumnNames))
+                .Add(DuplicateColumns.Validate(csvHeaderRecord))
+                .Add(InvalidColumns.Validate(csvHeaderRecord, RequiredColumnNames.AddRange(OptionalColumnNames)));
+
+            while (csv.Read())
+            {
+            }
         }
         catch (Exception ex)
         {
-            validationIssues = validationIssues.Add(new ValidationIssue($"Het bestand {filename} kon niet verwerkt worden. \n" +
-                                                                        "Gelieve het formaat te controleren. \n" +
-                                                                        "Contacteer de beheerders indien de fout zich opnieuw voordoet."));
-            logger.LogError(ex, "Error occured when validating imported CSV file {File}: {Message}", filename, ex.Message);
+            validationIssues = validationIssues.Add(
+                new ValidationIssue(
+                    $"Het bestand {filename} kon niet verwerkt worden. \n" +
+                    "Gelieve het formaat te controleren. \n" +
+                    "Contacteer de beheerders indien de fout zich opnieuw voordoet."));
+            logger.LogError(
+                ex,
+                "Error occured when validating imported CSV file {File}: {Message}",
+                filename,
+                ex.Message);
         }
 
         return validationIssues.ToResult();
--- test/OrganisationRegistry.UnitTests/Organisation/CreateOrganisationFromImport/WithoutRecords.cs
+++ test/OrganisationRegistry.UnitTests/Organisation/CreateOrganisationFromImport/WithoutRecords.cs
@@ -2,7 +2,6 @@
 
 using System;
 using System.Threading.Tasks;
-using FluentAssertions;
 using Microsoft.Extensions.Logging;
 using Moq;
 using OrganisationRegistry.Infrastructure.Domain;
