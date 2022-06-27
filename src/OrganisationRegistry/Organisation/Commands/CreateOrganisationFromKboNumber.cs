29f9699d2bf607c7dc4d897394c6a1f28f2be180
W0dan <W0dan.git@gmail.com>
2022-06-27 22:19:07 +0200

    feat: or-763 hyjack createOrgFromKbo

---
+++ src/OrganisationRegistry/Organisation/Commands/CreateOrganisationFromKboNumber.cs
@@ -0,0 +1,32 @@
+﻿namespace OrganisationRegistry.Organisation.Commands;
+
+public class CreateOrganisationFromKboNumber : BaseCommand<OrganisationId>
+{
+    public OrganisationId OrganisationId => Id;
+
+    public KboNumber KboNumber { get; }
+
+    public CreateOrganisationFromKboNumber(OrganisationId organisationId, KboNumber kboNumber)
+    {
+        Id = organisationId;
+        KboNumber = kboNumber;
+    }
+
+    public static implicit operator CreateOrganisationFromKbo(CreateOrganisationFromKboNumber command)
+        => new(
+            command.OrganisationId,
+            string.Empty,
+            null,
+            null,
+            Article.None,
+            null,
+            null,
+            null,
+            false,
+            new ValidFrom(),
+            new ValidTo(),
+            command.KboNumber,
+            new ValidFrom(),
+            new ValidTo()
+        );
+}
