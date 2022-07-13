88b3b3ea6e86b38fad19ece08572e1e81edfa5e7
Koen Metsu <koen@metsu.be>
2022-07-13 14:37:58 +0200

    feat: or-879 remove use of security service

--- src/OrganisationRegistry.Infrastructure/Authorization/IEditSecurityService.cs
+++
@@ -1,9 +0,0 @@
-namespace OrganisationRegistry.Infrastructure.Authorization;
-
-using System;
-
-public interface IEditSecurityService
-{
-    bool CanAddKey(Guid keyTypeId);
-    bool CanEditKey(Guid keyTypeId);
-}
