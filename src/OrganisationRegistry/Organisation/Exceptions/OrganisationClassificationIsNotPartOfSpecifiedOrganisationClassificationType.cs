c34cd1c60ecd32d65b5431242bb58a7d02b48f4e
QuintenGreenstack <quinten.vanassche@greenstack.be>
2022-07-18 14:42:07 +0200

    fix: or-887 correct error message

--- src/OrganisationRegistry/Organisation/Exceptions/OrganisationClassificationIsNotPartOfSpecifiedOrganisationClassificationType.cs
+++ src/OrganisationRegistry/Organisation/Exceptions/OrganisationClassificationIsNotPartOfSpecifiedOrganisationClassificationType.cs
@@ -3,7 +3,7 @@
 public class OrganisationClassificationIsNotPartOfSpecifiedOrganisationClassificationType:DomainException
 {
     public OrganisationClassificationIsNotPartOfSpecifiedOrganisationClassificationType()
-        : base("De opgegeven ogranisatie classificatie behoort niet tot het opgegeven organisatie classificatie type")
+        : base("De opgegeven organisatie classificatie behoort niet tot het opgegeven organisatie classificatie type")
     {
     }
 }
