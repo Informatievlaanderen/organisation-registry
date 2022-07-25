505c697342e9245e4d15caa7a1015af2ebded67d
QuintenGreenstack <quinten.vanassche@greenstack.be>
2022-07-25 14:50:46 +0200

    fix: or-1119 fix build

--- test/OrganisationRegistry.UnitTests/Organisation/AddOrganisationCapacity/WhenAddingAnOrganisationCapacityInTheFuture.cs
+++ test/OrganisationRegistry.UnitTests/Organisation/AddOrganisationCapacity/WhenAddingAnOrganisationCapacityInTheFuture.cs
@@ -3,29 +3,38 @@
 using System;
 using System.Threading.Tasks;
 using AutoFixture;
+using Capacity;
 using Capacity.Events;
+using FluentAssertions;
 using Infrastructure.Tests.Extensions.TestHelpers;
 using Microsoft.Extensions.Logging;
 using Moq;
 using OrganisationRegistry.Infrastructure.Domain;
-using OrganisationRegistry.Infrastructure.Events;
 using OrganisationRegistry.Organisation;
 using OrganisationRegistry.Organisation.Events;
+using Tests.Shared;
 using Tests.Shared.Stubs;
 using Tests.Shared.TestDataBuilders;
 using Xunit;
 using Xunit.Abstractions;
 
-public class WhenAddingAnOrganisationCapacity
+public class WhenAddingAnOrganisationCapacityInTheFuture
     : Specification<AddOrganisationCapacityCommandHandler, AddOrganisationCapacity>
 {
     private readonly Fixture _fixture;
-    private readonly Guid _capacityId;
+    private readonly DateTimeProviderStub _dateTimeProviderStub;
 
-    public WhenAddingAnOrganisationCapacity(ITestOutputHelper helper) : base(helper)
+    private readonly Guid _capacityId;
+    private readonly Guid _organisationId;
+    private readonly Guid _organisationCapacityId;
+
+    public WhenAddingAnOrganisationCapacityInTheFuture(ITestOutputHelper helper) : base(helper)
     {
         _fixture = new Fixture();
+        _dateTimeProviderStub = new DateTimeProviderStub(DateTime.Now);
         _capacityId = _fixture.Create<Guid>();
+        _organisationId = _fixture.Create<Guid>();
+        _organisationCapacityId = _fixture.Create<Guid>();
     }
 
     protected override AddOrganisationCapacityCommandHandler BuildHandler(ISession session)
@@ -33,20 +42,40 @@ public class WhenAddingAnOrganisationCapacity
             Mock.Of<ILogger<AddOrganisationCapacityCommandHandler>>(),
             session,
             new OrganisationRegistryConfigurationStub(),
-            new DateTimeProviderStub(DateTime.Now));
+            _dateTimeProviderStub);
+
+    private AddOrganisationCapacity AddOrganisationCapacityCommand
+        => new(
+            _organisationCapacityId,
+            new OrganisationId(_organisationId),
+            new CapacityId(_capacityId),
+            personId: null,
+            functionId: null,
+            locationId: null,
+            contacts: null,
+            new ValidFrom(_dateTimeProviderStub.Today.AddDays(value: 1)),
+            new ValidTo(_dateTimeProviderStub.Today.AddDays(value: 2))
+            );
 
     [Fact]
-    public async Task WhenInFuture_PublishesOneEvent()
+    public async Task PublishesOneEvent()
     {
-        Given(OrganisationCreated,
+        await Given(OrganisationCreated,
                 CapacityCreated)
-            .When(FutureOrganisationCapacityCreated)
+            .When(AddOrganisationCapacityCommand, TestUser.AlgemeenBeheerder)
             .ThenItPublishesTheCorrectNumberOfEvents(1);
+
+        var organisationCapacityAdded =  PublishedEvents[0].UnwrapBody<OrganisationCapacityAdded>();
+        organisationCapacityAdded.OrganisationCapacityId.Should().Be(_organisationCapacityId);
+        organisationCapacityAdded.CapacityId.Should().Be(_capacityId);
+        organisationCapacityAdded.OrganisationId.Should().Be(_organisationId);
+        organisationCapacityAdded.ValidFrom.Should().Be(_dateTimeProviderStub.Today.AddDays(value: 1));
+        organisationCapacityAdded.ValidTo.Should().Be(_dateTimeProviderStub.Today.AddDays(value: 2));
     }
 
     private CapacityCreated CapacityCreated
         => new(_capacityId, _fixture.Create<string>());
 
     private OrganisationCreated OrganisationCreated
-        => new OrganisationCreatedBuilder();
+        => new OrganisationCreatedBuilder().WithId(_organisationId);
 }
