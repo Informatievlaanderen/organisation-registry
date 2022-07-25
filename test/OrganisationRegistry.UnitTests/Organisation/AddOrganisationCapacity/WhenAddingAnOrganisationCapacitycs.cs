b81aff500360cdca4a0e66106033e6be0901cf1e
QuintenGreenstack <quinten.vanassche@greenstack.be>
2022-07-25 14:50:46 +0200

    fix: or-1119 fix build

--- test/OrganisationRegistry.UnitTests/Organisation/AddOrganisationCapacity/WhenAddingAnOrganisationCapacitycs.cs
+++ test/OrganisationRegistry.UnitTests/Organisation/AddOrganisationCapacity/WhenAddingAnOrganisationCapacitycs.cs
@@ -1,17 +1,26 @@
 ﻿namespace OrganisationRegistry.UnitTests.Organisation.AddOrganisationCapacity;
 
 using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using AutoFixture;
 using Capacity;
 using Capacity.Events;
+using ContactType;
+using ContactType.Events;
 using FluentAssertions;
+using Function;
+using Function.Events;
 using Infrastructure.Tests.Extensions.TestHelpers;
+using Location;
+using Location.Events;
 using Microsoft.Extensions.Logging;
 using Moq;
 using OrganisationRegistry.Infrastructure.Domain;
 using OrganisationRegistry.Organisation;
 using OrganisationRegistry.Organisation.Events;
+using Person;
+using Person.Events;
 using Tests.Shared;
 using Tests.Shared.Stubs;
 using Tests.Shared.TestDataBuilders;
@@ -27,6 +36,10 @@ public class WhenAddingAnOrganisationCapacity
     private readonly Guid _capacityId;
     private readonly Guid _organisationId;
     private readonly Guid _organisationCapacityId;
+    private readonly Guid _personId;
+    private readonly Guid _functionId;
+    private readonly Guid _locationId;
+    private readonly Guid _contactTypeId1;
 
     public WhenAddingAnOrganisationCapacity(ITestOutputHelper helper) : base(helper)
     {
@@ -35,6 +48,10 @@ public class WhenAddingAnOrganisationCapacity
         _capacityId = _fixture.Create<Guid>();
         _organisationId = _fixture.Create<Guid>();
         _organisationCapacityId = _fixture.Create<Guid>();
+        _personId = _fixture.Create<Guid>();
+        _functionId = _fixture.Create<Guid>();
+        _locationId = _fixture.Create<Guid>();
+        _contactTypeId1 = _fixture.Create<Guid>();
     }
 
     protected override AddOrganisationCapacityCommandHandler BuildHandler(ISession session)
@@ -49,29 +66,52 @@ public class WhenAddingAnOrganisationCapacity
             _organisationCapacityId,
             new OrganisationId(_organisationId),
             new CapacityId(_capacityId),
-            personId: null,
-            functionId: null,
-            locationId: null,
-            contacts: null,
-            new ValidFrom(_dateTimeProviderStub.Today.AddDays(value: 1)),
-            new ValidTo(_dateTimeProviderStub.Today.AddDays(value: 2))
-            );
+            new PersonId(_personId),
+            new FunctionTypeId(_functionId),
+            new LocationId(_locationId),
+            new Dictionary<ContactTypeId, string>
+            {
+                { new ContactTypeId(_contactTypeId1), _fixture.Create<string>() },
+            },
+            new ValidFrom(_dateTimeProviderStub.Yesterday),
+            new ValidTo(_dateTimeProviderStub.Today.AddDays(value: 1))
+        );
 
     [Fact]
-    public async Task PublishesOneEvent()
+    public async Task PublishesTwoEvent()
     {
-        await Given(OrganisationCreated,
-                CapacityCreated)
+        await Given(
+                OrganisationCreated,
+                CapacityCreated,
+                PersonCreated,
+                FunctionCreated,
+                LocationCreated,
+                ContactTypeCreated)
             .When(AddOrganisationCapacityCommand, TestUser.AlgemeenBeheerder)
-            .ThenItPublishesTheCorrectNumberOfEvents(1);
+            .ThenItPublishesTheCorrectNumberOfEvents(2);
 
-        var organisationCapacityAdded =  PublishedEvents[0].UnwrapBody<OrganisationCapacityAdded>();
+        var organisationCapacityAdded = PublishedEvents[0].UnwrapBody<OrganisationCapacityAdded>();
         organisationCapacityAdded.OrganisationCapacityId.Should().Be(_organisationCapacityId);
         organisationCapacityAdded.CapacityId.Should().Be(_capacityId);
         organisationCapacityAdded.OrganisationId.Should().Be(_organisationId);
+        organisationCapacityAdded.FunctionId.Should().Be(_functionId);
+        organisationCapacityAdded.LocationId.Should().Be(_locationId);
+        organisationCapacityAdded.PersonId.Should().Be(_personId);
+        organisationCapacityAdded.ValidFrom.Should().Be(_dateTimeProviderStub.Yesterday);
         organisationCapacityAdded.ValidTo.Should().Be(_dateTimeProviderStub.Today.AddDays(value: 1));
-        organisationCapacityAdded.ValidFrom.Should().Be(_dateTimeProviderStub.Today.AddDays(value: 2));
     }
+
+    private ContactTypeCreated ContactTypeCreated
+        => new(_contactTypeId1, _fixture.Create<string>());
+
+    private LocationCreated LocationCreated
+        => new(_locationId, null, _fixture.Create<string>(), _fixture.Create<string>(), _fixture.Create<string>(), _fixture.Create<string>(), _fixture.Create<string>());
+
+    private FunctionCreated FunctionCreated
+        => new(_functionId, _fixture.Create<string>());
+
+    private PersonCreated PersonCreated
+        => new(_personId, _fixture.Create<string>(), _fixture.Create<string>(), null, null);
 
     private CapacityCreated CapacityCreated
         => new(_capacityId, _fixture.Create<string>());
