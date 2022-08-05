888a73d0460161aa2a66ac6f9f308855efaf7492
QuintenGreenstack <quinten.vanassche@greenstack.be>
2022-08-05 16:09:06 +0200

    fix: or-1139 show username and role on login

--- src/OrganisationRegistry.UI/app/body/create/create.component.ts
+++ src/OrganisationRegistry.UI/app/body/create/create.component.ts
@@ -1,23 +1,23 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
+import { Component, OnInit, OnDestroy } from "@angular/core";
+import { ActivatedRoute, Router } from "@angular/router";
 
-import { Subscription } from 'rxjs/Subscription';
+import { Subscription } from "rxjs/Subscription";
 
-import {OidcService} from 'core/auth';
-import { AlertService} from 'core/alert';
+import { OidcService } from "core/auth";
+import { AlertService } from "core/alert";
 
-import { BodyService } from 'services/bodies';
-import { OrganisationService } from 'services/organisations';
+import { BodyService } from "services/bodies";
+import { OrganisationService } from "services/organisations";
 
-import { SearchResult } from 'shared/components/form/form-group-autocomplete';
+import { SearchResult } from "shared/components/form/form-group-autocomplete";
 
-import { CreateChildAlerts } from './create.alerts';
+import { CreateChildAlerts } from "./create.alerts";
 
-import { CreateBodyFormValues } from './create-body-form';
+import { CreateBodyFormValues } from "./create-body-form";
 
 @Component({
-  templateUrl: 'create.template.html',
-  styleUrls: ['create.style.css']
+  templateUrl: "create.template.html",
+  styleUrls: ["create.style.css"],
 })
 export class CreateBodyComponent implements OnInit, OnDestroy {
   public isBusy: boolean = false;
@@ -33,49 +33,59 @@ export class CreateBodyComponent implements OnInit, OnDestroy {
     private bodyService: BodyService,
     private organisationService: OrganisationService,
     private alertService: AlertService,
-    private oidcService: OidcService) { }
+    private oidcService: OidcService
+  ) {}
 
   ngOnInit() {
     this.body = new CreateBodyFormValues();
     this.subscriptions.push(
-      this.route
-        .queryParams
-        .subscribe(params => {
-          if (params['organisationId']) {
-            this.body.organisationId = params['organisationId'];
-            this.isBusy = true;
-            this.subscriptions.push(this.organisationService
+      this.route.queryParams.subscribe((params) => {
+        if (params["organisationId"]) {
+          this.body.organisationId = params["organisationId"];
+          this.isBusy = true;
+          this.subscriptions.push(
+            this.organisationService
               .get(this.body.organisationId)
-              .finally(() => this.isBusy = false)
-              .subscribe(result => this.organisation = new SearchResult(result.id, result.name)));
-          }
-        }));
+              .finally(() => (this.isBusy = false))
+              .subscribe(
+                (result) =>
+                  (this.organisation = new SearchResult(result.id, result.name))
+              )
+          );
+        }
+      })
+    );
   }
 
   ngOnDestroy() {
-    this.subscriptions.forEach(sub => sub.unsubscribe());
+    this.subscriptions.forEach((sub) => sub.unsubscribe());
   }
 
   create(value: CreateBodyFormValues) {
     this.isBusy = true;
-    this.subscriptions.push(this.bodyService.create(value)
-      .finally(() => this.isBusy = false)
-      .subscribe(
-        result => this.onCreateSuccess(result, value),
-        error => this.alertService.setAlert(this.createAlerts.saveError(error))
-      ));
+    this.subscriptions.push(
+      this.bodyService
+        .create(value)
+        .finally(() => (this.isBusy = false))
+        .subscribe(
+          (result) => this.onCreateSuccess(result, value),
+          (error) =>
+            this.alertService.setAlert(this.createAlerts.saveError(error))
+        )
+    );
   }
 
   private onCreateSuccess(result, value) {
     if (result) {
-      this.oidcService.resetSecurityCache();
+      this.oidcService.updateFromServer().subscribe();
 
-      this.router.navigate(['./../', value.id], { relativeTo: this.route });
+      this.router.navigate(["./../", value.id], { relativeTo: this.route });
 
       let bodyUrl = this.router.serializeUrl(
-        this.router.createUrlTree(
-          ['./../', value.id],
-          { relativeTo: this.route }));
+        this.router.createUrlTree(["./../", value.id], {
+          relativeTo: this.route,
+        })
+      );
 
       this.alertService.setAlert(this.createAlerts.saveSuccess(value, bodyUrl));
     }
