7321898a86f9de619dec48b85a8552e7eb11d99a
QuintenGreenstack <quinten.vanassche@greenstack.be>
2022-08-23 08:58:50 +0200

    feat: or-1140 add final unit tests

--- test/OrganisationRegistry.UnitTests/Organisation/UpdateOrganisationRegulation/WhenUpdatingAnOrganisationRegulation.cs
+++ test/OrganisationRegistry.UnitTests/Organisation/UpdateOrganisationRegulation/WhenUpdatingAnOrganisationRegulation.cs
@@ -1,5 +1,97 @@
 ﻿namespace OrganisationRegistry.UnitTests.Organisation.UpdateOrganisationRegulation;
 
-public class WhenUpdatingAnOrganisationRegulation
+using System;
+using System.Threading.Tasks;
+using AutoFixture;
+using FluentAssertions;
+using Infrastructure.Tests.Extensions.TestHelpers;
+using Microsoft.Extensions.Logging;
+using Moq;
+using OrganisationRegistry.Infrastructure.Domain;
+using OrganisationRegistry.Organisation;
+using OrganisationRegistry.Organisation.Events;
+using RegulationSubTheme;
+using RegulationSubTheme.Events;
+using RegulationTheme;
+using RegulationTheme.Events;
+using Tests.Shared;
+using Tests.Shared.TestDataBuilders;
+using Xunit;
+using Xunit.Abstractions;
+
+public class WhenUpdatingAnOrganisationRegulation : Specification<UpdateOrganisationRegulationCommandHandler, UpdateOrganisationRegulation>
 {
+    private readonly Fixture _fixture;
+    private readonly Guid _organisationId;
+    private readonly Guid _regulationThemeId;
+    private readonly Guid _regulationSubThemeId;
+    private readonly Guid _organisationRegulationId;
+
+    public WhenUpdatingAnOrganisationRegulation(ITestOutputHelper helper) : base(helper)
+    {
+        _fixture = new Fixture();
+        _organisationId = _fixture.Create<Guid>();
+        _regulationThemeId = _fixture.Create<Guid>();
+        _regulationSubThemeId = _fixture.Create<Guid>();
+        _organisationRegulationId = _fixture.Create<Guid>();
+    }
+
+    protected override UpdateOrganisationRegulationCommandHandler BuildHandler(ISession session)
+        => new(Mock.Of<ILogger<UpdateOrganisationRegulationCommandHandler>>(), session);
+
+    private UpdateOrganisationRegulation UpdateOrganisationRegulationCommand
+        => new(
+            _organisationRegulationId,
+            new OrganisationId(_organisationId),
+            new RegulationThemeId(_regulationThemeId),
+            new RegulationSubThemeId(_regulationSubThemeId),
+            _fixture.Create<string>(),
+            null,
+            null,
+            null,
+            null,
+            null,
+            new ValidFrom(),
+            new ValidTo());
+
+    [Fact]
+    public async Task PublishesOneEvent()
+    {
+        await Given(OrganisationCreated, RegulationThemeCreated, RegulationSubThemeCreated, OrganisationRegulationAdded)
+            .When(UpdateOrganisationRegulationCommand, TestUser.AlgemeenBeheerder)
+            .ThenItPublishesTheCorrectNumberOfEvents(1);
+
+       var organisationRegulationUpdate =  PublishedEvents[0].UnwrapBody<OrganisationRegulationUpdated>();
+       organisationRegulationUpdate.OrganisationId.Should().Be(_organisationId);
+       organisationRegulationUpdate.OrganisationRegulationId.Should().Be(_organisationRegulationId);
+       organisationRegulationUpdate.RegulationThemeId.Should().Be(_regulationThemeId);
+       organisationRegulationUpdate.RegulationSubThemeId.Should().Be(_regulationSubThemeId);
+    }
+
+    private OrganisationCreated OrganisationCreated
+        => new OrganisationCreatedBuilder().WithId(_organisationId);
+
+    private RegulationThemeCreated RegulationThemeCreated
+        => new(_regulationThemeId, _fixture.Create<string>());
+
+    private RegulationSubThemeCreated RegulationSubThemeCreated
+        => new(_regulationSubThemeId, _fixture.Create<string>(), _regulationThemeId, _fixture.Create<string>());
+
+    private OrganisationRegulationAdded OrganisationRegulationAdded
+        => new(
+            _organisationId,
+            _organisationRegulationId,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null);
 }
+
---
+++ test/OrganisationRegistry.UnitTests/Organisation/UpdateOrganisationRelation/WhenUpdatingAnOrganisationRelation.cs
@@ -0,0 +1,84 @@
+﻿namespace OrganisationRegistry.UnitTests.Organisation.UpdateOrganisationRegulation;
+
+using System;
+using System.Threading.Tasks;
+using AutoFixture;
+using FluentAssertions;
+using Infrastructure.Tests.Extensions.TestHelpers;
+using Microsoft.Extensions.Logging;
+using Moq;
+using OrganisationRegistry.Infrastructure.Domain;
+using OrganisationRegistry.Organisation;
+using OrganisationRegistry.Organisation.Events;
+using OrganisationRelationType;
+using OrganisationRelationType.Events;
+using Tests.Shared;
+using Tests.Shared.TestDataBuilders;
+using Xunit;
+using Xunit.Abstractions;
+
+public class WhenUpdatingAnOrganisationRelation : Specification<UpdateOrganisationRelationCommandHandler, UpdateOrganisationRelation>
+{
+    private readonly Fixture _fixture;
+    private readonly Guid _firstOrganisationId;
+    private readonly Guid _secondOrganisationId;
+    private readonly Guid _organisationRelationTypeId;
+    private readonly Guid _organisationRelationId;
+
+    public WhenUpdatingAnOrganisationRelation(ITestOutputHelper helper) : base(helper)
+    {
+        _fixture = new Fixture();
+        _firstOrganisationId = _fixture.Create<Guid>();
+        _secondOrganisationId = _fixture.Create<Guid>();
+        _organisationRelationTypeId = _fixture.Create<Guid>();
+        _organisationRelationId = _fixture.Create<Guid>();
+    }
+
+    protected override UpdateOrganisationRelationCommandHandler BuildHandler(ISession session)
+        => new(Mock.Of<ILogger<UpdateOrganisationRelationCommandHandler>>(), session);
+
+    private UpdateOrganisationRelation UpdateOrganisationRelationCommand
+        => new(
+            _organisationRelationId,
+            new OrganisationRelationTypeId(_organisationRelationTypeId),
+            new OrganisationId(_firstOrganisationId),
+            new OrganisationId(_secondOrganisationId),
+            new ValidFrom(),
+            new ValidTo());
+
+    [Fact]
+    public async Task PublishesOneEvent()
+    {
+        await Given(FirstOrganisationCreated, SecondOrganisationCreated, OrganisationRelationTypeCreated, OrganisationRelationAdded)
+            .When(UpdateOrganisationRelationCommand, TestUser.AlgemeenBeheerder)
+            .ThenItPublishesTheCorrectNumberOfEvents(1);
+
+        var organisationRegulationUpdate = PublishedEvents[0].UnwrapBody<OrganisationRelationUpdated>();
+        organisationRegulationUpdate.OrganisationId.Should().Be(_firstOrganisationId);
+        organisationRegulationUpdate.RelationId.Should().Be(_organisationRelationTypeId);
+        organisationRegulationUpdate.RelatedOrganisationId.Should().Be(_secondOrganisationId);
+        organisationRegulationUpdate.OrganisationRelationId.Should().Be(_organisationRelationId);
+    }
+
+    private OrganisationCreated FirstOrganisationCreated
+        => new OrganisationCreatedBuilder().WithId(_firstOrganisationId);
+
+    private OrganisationCreated SecondOrganisationCreated
+        => new OrganisationCreatedBuilder().WithId(_secondOrganisationId);
+
+    private OrganisationRelationTypeCreated OrganisationRelationTypeCreated
+        => new(_organisationRelationTypeId, _fixture.Create<string>(), null);
+
+    private OrganisationRelationAdded OrganisationRelationAdded
+        => new(
+            _firstOrganisationId,
+            _fixture.Create<string>(),
+            _organisationRelationId,
+            _secondOrganisationId,
+            _fixture.Create<string>(),
+            _organisationRelationTypeId,
+            _fixture.Create<string>(),
+            _fixture.Create<string>(),
+            null,
+            null);
+}
