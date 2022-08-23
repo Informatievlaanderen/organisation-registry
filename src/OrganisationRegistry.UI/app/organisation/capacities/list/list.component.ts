8d7432cd25eafc9e3de9cce54b3e992676a4aa90
QuintenGreenstack <quinten.vanassche@greenstack.be>
2022-08-23 09:41:45 +0200

    fix: or-1070 guard remove capacities in UI

--- src/OrganisationRegistry.UI/app/organisation/capacities/list/list.component.ts
+++ src/OrganisationRegistry.UI/app/organisation/capacities/list/list.component.ts
@@ -1,19 +1,19 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
-import { Observable } from 'rxjs/Observable';
+import { Component, Input, Output, EventEmitter } from "@angular/core";
+import { Observable } from "rxjs/Observable";
 
-import { BaseListComponent } from 'shared/components/list';
-import { OrganisationCapacityListItem } from 'services/organisationcapacities';
+import { BaseListComponent } from "shared/components/list";
+import { OrganisationCapacityListItem } from "services/organisationcapacities";
 
 @Component({
-  selector: 'ww-organisation-capacity-list',
-  templateUrl: 'list.template.html',
-  styleUrls: ['list.style.css'],
-  inputs: ['items', 'isBusy'],
-  outputs: ['changePage']
+  selector: "ww-organisation-capacity-list",
+  templateUrl: "list.template.html",
+  styleUrls: ["list.style.css"],
+  inputs: ["items", "isBusy"],
+  outputs: ["changePage"],
 })
 export class OrganisationCapacitiesListComponent extends BaseListComponent<OrganisationCapacityListItem> {
-  @Input('canEdit') canEdit: Observable<boolean>;
-
+  @Input("canEdit") canEdit: Observable<boolean>;
+  @Input("canDelete") canDelete: Observable<boolean>;
   @Output()
   removeCapacityClicked: EventEmitter<OrganisationCapacityListItem> = new EventEmitter<OrganisationCapacityListItem>();
 
