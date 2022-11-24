6d0f4977f68eedcf3fa8e964c86f839e8a724865
Koen Metsu <koen@metsu.be>
2022-11-24 16:33:50 +0100

    feat: enable moar parallellisation

--- build.sh
+++ build.sh
@@ -9,7 +9,7 @@ FAKE_ALLOW_NO_DEPENDENCIES=true dotnet fake build -t "CleanAll"
 
 if [ $# -eq 0 ]
 then
-  FAKE_ALLOW_NO_DEPENDENCIES=true dotnet fake build --parallel 2
+  FAKE_ALLOW_NO_DEPENDENCIES=true dotnet fake build --parallel 8
 else
-  FAKE_ALLOW_NO_DEPENDENCIES=true dotnet fake build --parallel 2 -t "$@"
+  FAKE_ALLOW_NO_DEPENDENCIES=true dotnet fake build --parallel 8 -t "$@"
 fi
