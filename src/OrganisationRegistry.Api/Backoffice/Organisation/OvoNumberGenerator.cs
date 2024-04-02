1f3367b7ccbf443f96f871fd5fd1824014451df9
Koen Metsu <koen@metsu.be>
2024-04-02 12:04:36 +0200

    feat: try to improve first org creation perf

--- src/OrganisationRegistry.Api/Backoffice/Organisation/OvoNumberGenerator.cs
+++ src/OrganisationRegistry.Api/Backoffice/Organisation/OvoNumberGenerator.cs
@@ -1,12 +1,13 @@
 ﻿namespace OrganisationRegistry.Api.Backoffice.Organisation;
 
 using System.Linq;
+using Microsoft.EntityFrameworkCore;
 using OrganisationRegistry.Organisation;
 using SqlServer.Infrastructure;
 
 public class OvoNumberGenerator : NumberGenerator, IOvoNumberGenerator
 {
     public OvoNumberGenerator(OrganisationRegistryContext context)
-        : base("OVO", "Organisatie nummer", () => context.OrganisationDetail.Max(item => item.OvoNumber))
+        : base("OVO", "Organisatie nummer", () => context.OrganisationDetail.AsNoTracking().Max(item => item.OvoNumber))
     { }
 }
