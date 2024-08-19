c2c895558b532b4c588863194c3cb1be65314f03
Jan Lesage <jan.lesage@vlaanderen.be>
2024-08-19 15:32:42 +0200

    fix: or-1894 return bad request instead of server error when scroll timed out

---
+++ src/OrganisationRegistry.Api/Infrastructure/ElasticsearchScrollTimeoutException.cs
@@ -0,0 +1,18 @@
+﻿namespace OrganisationRegistry.Api.Infrastructure;
+
+using System;
+
+public class ElasticsearchScrollTimeoutException : DomainException
+{
+    public ElasticsearchScrollTimeoutException()
+    {
+    }
+
+    public ElasticsearchScrollTimeoutException(string message) : base(message)
+    {
+    }
+
+    public ElasticsearchScrollTimeoutException(string message, Exception inner) : base(message, inner)
+    {
+    }
+}
