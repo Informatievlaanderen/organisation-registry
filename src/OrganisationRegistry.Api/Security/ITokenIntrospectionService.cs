596173a92bfeb5fad3c317b6d336f08ff6248c55
Sadik-Dev <oussama.sadik@vlaanderen.be>
2026-05-05 14:21:37 +0200

    chore: remove generated demo output and unused introspection service

--- src/OrganisationRegistry.Api/Security/ITokenIntrospectionService.cs
+++
@@ -1,8 +0,0 @@
-namespace OrganisationRegistry.Api.Security;
-
-using System.Threading.Tasks;
-
-public interface ITokenIntrospectionService
-{
-    Task<IntrospectionResponse> IntrospectTokenAsync(string token);
-}
