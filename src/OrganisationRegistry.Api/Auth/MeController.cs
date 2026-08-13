f9535cf691c34313f8cc3be60eadb159933c16b8
Yusuf Mavzer <yusuf.mavzer@vlaanderen.be>
2026-08-13 10:23:22 +0200

    fix: finalize ticket #OR-3297

--- src/OrganisationRegistry.Api/Auth/MeController.cs
+++ src/OrganisationRegistry.Api/Auth/MeController.cs
@@ -8,38 +8,39 @@ using Microsoft.AspNetCore.Mvc;
 using Infrastructure;
 using Infrastructure.Swagger.Examples;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
 using OrganisationRegistry.Api.Infrastructure.Security;
 using OrganisationRegistry.Infrastructure;
 using OrganisationRegistry.Infrastructure.Authorization;
 using Responses;
 using Swashbuckle.AspNetCore.Filters;
 using ApiException = Be.Vlaanderen.Basisregisters.Api.Exceptions.ApiException;
-using ForbiddenResponseExamples = Infrastructure.Swagger.Examples.ForbiddenResponseExamples;
 
 [ApiVersion("1.0")]
 [AdvertiseApiVersions("1.0")]
 [ApiExplorerSettings(GroupName = "Authorization")]
 [OrganisationRegistryRoute("me")]
-[OrganisationRegistryAuthorize]
 [Consumes("application/json")]
 [Produces("application/json")]
-public class MeController(ISecurityService securityService) : OrganisationRegistryController
+public class MeController : OrganisationRegistryController
 {
+    private readonly ILogger<MeController> _logger;
+
+    public MeController(ILogger<MeController> logger)
+    {
+        _logger = logger;
+    }
     /// <summary>Gegevens van de huidige gebruiker.</summary>
     /// <remarks>Haalt de gegevens op van de gebruiker die momenteel aangemeld is.</remarks>
     /// <response code="200">De gegevens van de aangemelde gebruiker zijn opgehaald.</response>
-    /// <response code="401">De authenticatie is ongeldig of verlopen.</response>
-    /// <response code="403">De gebruiker beschikt niet over een geldige Wegwijs-rol.</response>
     /// <response code="500">Er is een interne fout opgetreden.</response>
     [HttpGet]
+    [OrProtected]
     [ProducesResponseType(typeof(MeResponse), StatusCodes.Status200OK)]
-    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
-    [ProducesResponseType(StatusCodes.Status403Forbidden)]
     [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status500InternalServerError)]
     [SwaggerResponseExample(StatusCodes.Status200OK, typeof(MeResponsOkExamples))]
-    [SwaggerResponseExample(StatusCodes.Status403Forbidden, typeof(ForbiddenResponseExamples))]
     [SwaggerResponseExample(StatusCodes.Status500InternalServerError, typeof(InternalServerErrorResponseExamples))]
-    public async Task<ActionResult<MeResponse>> Get()
+    public async Task<ActionResult<MeResponse>> Get([FromServices]ISecurityService securityService)
     {
         try
         {
